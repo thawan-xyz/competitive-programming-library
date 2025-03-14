36b0c3288a8155957ee59bfc08585c8cd533e3a3
Thawan <thawan.xyz@gmail.com>
2025-03-14 12:43:21 -0300

    base

---
+++ base/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define array vector
+#define read cin
+#define str string
+#define write cout
+#define endl '\n'
+using namespace std;
+
+/* author: Thawan Silva, 2024-08-14
+ * problem: [14] - Name
+ * url: https://github.com/thawan-xyz */
+
+int main() {
+    write << "Thawan" << endl;
+    return 0;
+}
