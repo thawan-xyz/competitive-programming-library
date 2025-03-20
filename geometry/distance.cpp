2e4fc6939ac61ad8e428951a3d3ac2e3b62d4fcf
Thawan <thawan.xyz@gmail.com>
2025-03-20 08:33:28 -0300

    geometry

--- geometry/distance.cpp
+++ geometry/distance.cpp
@@ -1,3 +1,7 @@
-float point_line(point s1, point s2, point p) {
+float point_point(point p, point q) {
+    return norm(p - q);
+}
+
+float point_line(point p, point s1, point s2) {
     return abs(cross(s1, s2, p)) / norm(s1 - s2);
 }
