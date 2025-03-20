f841aca73c26880fa75b617c59f17b8d7e25d639
Thawan <thawan.xyz@gmail.com>
2025-03-20 08:06:12 -0300

    geometry

--- geometry/orientation.cpp
+++ geometry/orientation.cpp
@@ -1,4 +1,4 @@
 int orient(point &p1, point &p2, point &p3) {
-    int cross = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
-    return (cross > 0) ? -1 : ((cross == 0) ? 0 : 1);
+    int c = cross(p1, p2, p3);
+    return (c > 0) ? -1 : ((c == 0) ? 0 : 1);
 }
