dbb309504709d0b290a981113fe9b9fd8264e324
thawan-xyz <thawan.xyz@gmail.com>
2025-04-20 02:45:32 -0300

    extra

--- extra/hanoi.cpp
+++ extra/hanoi.cpp
@@ -1,6 +1,6 @@
 void hanoi(int n, char l, char m, char r) {
     if (n == 0) return;
     hanoi(n - 1, l, r, m);
-    stdout << "Move disk " << n << " from " << l << " to " << r << endl;
+    write << "Move disk " << n << " from " << l << " to " << r << endl;
     hanoi(n - 1, m, l, r);
 }
