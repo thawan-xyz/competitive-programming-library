d6562c03be531bc6780f55fb5424df3692210442
thawan-xyz <thawan.xyz@gmail.com>
2025-04-20 02:01:44 -0300

    base

--- base/main.cpp
+++ base/main.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 #define array vector
-#define stdin cin
+#define read cin
 #define str string
-#define stdout cout
+#define write cout
 #define endl '\n'
 using namespace std;
+#define repeat(x) while(x--)
 
 /* author: Thawan, 2024-08-14
  * problem: [14] - Name
  * url: https://github.com/thawan-xyz */
 
-int main() {
-    stdout << "Thawan" << endl;
+void solve() {}
+
+signed main() {
+    ios::sync_with_stdio(false), read.tie(nullptr), write.tie(nullptr);
+    write << "Thawan" << endl;
     return 0;
 }
