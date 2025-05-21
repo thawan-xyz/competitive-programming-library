ffe1c906f680038dc9fa55e1737d5370df816531
thawan-xyz <thawan.xyz@gmail.com>
2025-05-21 13:00:31 -0300

    graph

---
+++ graph/articulation_and_bridges.cpp
@@ -0,0 +1,27 @@
+void articulation_and_bridges() {
+    int timer = 0;
+    array<array<int>> graph;
+    array<int> tin, low;
+    array<bool> visited, articulation;
+    array<pair<int, int>> bridges;
+    
+    function<void(int, int)> dfs = [&](int a, int p) -> void {
+        visited[a] = true;
+        tin[a] = low[a] = timer++;
+        
+        for (int b : graph[a]) {
+            if (b == p) continue;
+            
+            if (visited[b]) {
+                low[a] = min(low[a], tin[b]);
+            } else {
+                dfs(b, a);
+                low[a] = min(low[a], low[b]);
+                
+                if (tin[a] < low[b]) bridges.emplace_back(a, b);
+                if (p != -1 and tin[a] <= low[b]) articulation[a] = true;
+            }
+        }
+        if (p == -1 and graph[a].size() > 1) articulation[a] = true;
+    };
+}
