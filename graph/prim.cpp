324d62d7334cf9abed9c94fb0257cc01914140df
thawan-xyz <thawan.xyz@gmail.com>
2025-05-23 09:50:17 -0300

    graph

--- graph/prim.cpp
+++ graph/prim.cpp
@@ -8,20 +8,20 @@ graph prim(int n, graph &g) {
     array<bool> visited(n);
     array<long> distance(n, inf);
     priority_queue<pair<long, int>> p;
-    
+
     distance[0] = 0;
     p.emplace(0, 0);
     while (p.size() > 0) {
         auto [d, a] = p.top(); p.pop();
         if (visited[a]) continue;
-        
+
         visited[a] = true; d *= -1;
         cost += d;
         if (parent[a] != -1) {
             mst[parent[a]].emplace_back(a, d);
             mst[a].emplace_back(parent[a], d);
         }
-        
+
         for (auto &[b, w] : g[a]) {
             if (not visited[b] and w < distance[b]) {
                 distance[b] = w;
