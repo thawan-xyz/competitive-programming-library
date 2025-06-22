9bd5960d6c1b0f8732fa1fb064c5e0fa5c1d50bc
thawan-xyz <thawan.xyz@gmail.com>
2025-06-22 07:45:33 -0300

    structure

---
+++ structure/table.cpp
@@ -0,0 +1,13 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+const int random = chrono::high_resolution_clock::now().time_since_epoch().count();
+
+struct rhash {
+    int operator()(const int n) const {
+        return n ^ random;
+    }
+};
+
+template <typename K, typename V>
+using table = gp_hash_table<K, V, rhash>;
