8cdf288df84214ca8a55ce48190bc555a0aa6d78
thawan-xyz <thawan.xyz@gmail.com>
2025-06-24 22:25:28 -0300

    extra

--- extra/hanoi.cpp
+++ extra/hanoi.cpp
@@ -1,6 +1,6 @@
-function<void(int, char, char, char)> hanoi = [](int n, char l, char m, char r) {
+void hanoi(int n, char l, char m, char r) {
     if (n == 0) return;
     hanoi(n - 1, l, r, m);
     print << "Move disk " << n << " from " << l << " to " << r << '\n';
     hanoi(n - 1, m, l, r);
-};
+}
