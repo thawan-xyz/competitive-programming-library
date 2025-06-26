af160c37e7cb13aec23ff47d8f7b81a1fb77c39a
thawan-xyz <thawan.xyz@gmail.com>
2025-06-26 14:08:10 -0300

    tree

--- tree/lazy-segment-tree.cpp
+++ tree/lazy-segment-tree.cpp
@@ -12,16 +12,26 @@ struct lazy_segment_tree {
         build(a);
     }
 
+    int left(int n) {
+        return (n << 1) + 1;
+    }
+
+    int right(int n) {
+        return (n << 1) + 2;
+    }
+
     void build(array<T> &a, int l = 0, int r = -1, int n = 0) {
         if (r < 0) r = size - 1;
 
         if (l == r) {
             tree[n] = a[l];
-        } else {
-            int m = (l + r) / 2;
-            build(a, l, m, (2 * n) + 1), build(a, m + 1, r, (2 * n) + 2);
-            tree[n] = combine(tree[(2 * n) + 1], tree[(2 * n) + 2]);
+            return;
         }
+
+        int m = l + (r - l) / 2;
+        build(a, l, m, left(n));
+        build(a, m + 1, r, right(n));
+        tree[n] = combine(tree[left(n)], tree[right(n)]);
     }
 
     void push(int l, int r, int n) {
@@ -29,8 +39,8 @@ struct lazy_segment_tree {
 
         tree[n] = apply(tree[n], lazy[n], (r - l) + 1);
         if (l != r) {
-            lazy[(2 * n) + 1] = combine(lazy[(2 * n) + 1], lazy[n]);
-            lazy[(2 * n) + 2] = combine(lazy[(2 * n) + 2], lazy[n]);
+            lazy[left(n)] = combine(lazy[left(n)], lazy[n]);
+            lazy[right(n)] = combine(lazy[right(n)], lazy[n]);
         }
         lazy[n] = empty;
     }
@@ -38,28 +48,28 @@ struct lazy_segment_tree {
     void update(int queryl, int queryr, T v, int l = 0, int r = -1, int n = 0) {
         if (r < 0) r = size - 1;
         if (queryr < l or queryl > r) return;
-        push(l, r, n);
 
+        push(l, r, n);
         if (queryl <= l and queryr >= r) {
             lazy[n] = combine(lazy[n], v);
             push(l, r, n);
-        } else {
-            int m = (l + r) / 2;
-            update(queryl, queryr, v, l, m, (2 * n) + 1), update(queryl, queryr, v, m + 1, r, (2 * n) + 2);
-            tree[n] = combine(tree[(2 * n) + 1], tree[(2 * n) + 2]);
+            return;
         }
+
+        int m = l + (r - l) / 2;
+        update(queryl, queryr, v, l, m, left(n));
+        update(queryl, queryr, v, m + 1, r, right(n));
+        tree[n] = combine(tree[left(n)], tree[right(n)]);
     }
 
     T query(int queryl, int queryr, int l = 0, int r = -1, int n = 0) {
         if (r < 0) r = size - 1;
         if (l > queryr or r < queryl) return neutral;
-        push(l, r, n);
 
-        if (queryl <= l and queryr >= r) {
-            return tree[n];
-        } else {
-            int m = (l + r) / 2;
-            return combine(query(queryl, queryr, l, m, (2 * n) + 1), query(queryl, queryr, m + 1, r, (2 * n) + 2));
-        }
+        push(l, r, n);
+        if (queryl <= l and queryr >= r) return tree[n];
+
+        int m = l + (r - l) / 2;
+        return combine(query(queryl, queryr, l, m, left(n)), query(queryl, queryr, m + 1, r, right(n)));
     }
 };
