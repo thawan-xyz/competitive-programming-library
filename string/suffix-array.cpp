0385829b8307c4642c7e63f07a5a09526a60b8de
thawan-xyz <thawan.xyz@gmail.com>
2025-08-01 19:00:11 -0300

    string

--- string/suffix-array.cpp
+++ string/suffix-array.cpp
@@ -1,10 +1,10 @@
 struct suffix_array {
     int n;
-    str &s;
+    str s;
     array<int> suffix, lcp;
     array<int> rank, temp;
 
-    suffix_array(str &s): n(s.length()), s(s), suffix(n), lcp(n), rank(n), temp(n) {
+    suffix_array(str &base): n(base.length() + 1), s(base + '$'), suffix(n), lcp(n), rank(n), temp(n) {
         for (int i = 0; i < n; ++i) {
             suffix[i] = i;
             rank[i] = s[i];
@@ -62,5 +62,46 @@ struct suffix_array {
                 l = max(0, l - 1);
             }
         }
+    }
+
+    int compare(int i, str &t) {
+        return s.compare(i, t.size(), t);
+    }
+
+    int lower_search(str &t) {
+        int answer = 0;
+        int low = 1, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (compare(suffix[mid], t) < 0) {
+                low = mid + 1;
+            } else {
+                answer = mid;
+                high = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    int upper_search(str &t) {
+        int answer = n;
+        int low = 1, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (compare(suffix[mid], t) <= 0) {
+                low = mid + 1;
+            } else {
+                answer = mid;
+                high = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    int search(str &t) {
+        int lower = lower_search(t);
+        if (compare(suffix[lower], t) != 0) return 0;
+        int upper = upper_search(t);
+        return upper - lower;
     }
 };
