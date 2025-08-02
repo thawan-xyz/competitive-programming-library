f18875c7727588ca2478d33e6006256337132842
thawan-xyz <thawan.xyz@gmail.com>
2025-08-01 21:34:30 -0300

    string

--- string/suffix-array.cpp
+++ string/suffix-array.cpp
@@ -1,20 +1,20 @@
 struct suffix_array {
     int n;
     str s;
-    array<int> suffix, lcp;
+    array<int> suf, lcp;
     array<int> rank, temp;
 
-    suffix_array(str &base): n(base.length() + 1), s(base + '$'), suffix(n), lcp(n), rank(n), temp(n) {
+    suffix_array(str &base): n(base.length() + 1), s(base + '$'), suf(n), lcp(n), rank(n), temp(n) {
         for (int i = 0; i < n; ++i) {
-            suffix[i] = i;
+            suf[i] = i;
             rank[i] = s[i];
         }
         for (int k = 1; k < n; k <<= 1) {
             counting_sort(k), counting_sort(0);
-            temp[suffix[0]] = 1;
+            temp[suf[0]] = 1;
             for (int i = 1; i < n; ++i) {
-                int prev = suffix[i - 1];
-                int curr = suffix[i];
+                int prev = suf[i - 1];
+                int curr = suf[i];
                 if (rank[prev] == rank[curr] and rank[(prev + k) % n] == rank[(curr + k) % n]) {
                     temp[curr] = temp[prev];
                 } else {
@@ -22,7 +22,7 @@ struct suffix_array {
                 }
             }
             swap(rank, temp);
-            if (rank[suffix[n - 1]] == n) break;
+            if (rank[suf[n - 1]] == n) break;
         }
     }
 
@@ -30,28 +30,28 @@ struct suffix_array {
         int m = max(255, n) + 1;
         array<int> f(m);
         for (int i = 0; i < n; ++i) {
-            int r = rank[(suffix[i] + k) % n];
+            int r = rank[(suf[i] + k) % n];
             f[r] += 1;
         }
         for (int i = 1; i < m; ++i) {
             f[i] += f[i - 1];
         }
         for (int i = n - 1; i >= 0; --i) {
-            int r = rank[(suffix[i] + k) % n];
+            int r = rank[(suf[i] + k) % n];
             f[r] -= 1;
-            temp[f[r]] = suffix[i];
+            temp[f[r]] = suf[i];
         }
-        swap(suffix, temp);
+        swap(suf, temp);
     }
 
     void longest_common_prefix() {
         array<int> inv(n);
         for (int i = 0; i < n; ++i) {
-            inv[suffix[i]] = i;
+            inv[suf[i]] = i;
         }
         int l = 0;
         for (int i = 0; i < n - 1; ++i) {
-            int j = suffix[inv[i] - 1];
+            int j = suf[inv[i] - 1];
             while (s[i + l] == s[j + l]) l++;
             lcp[inv[i]] = l;
             l = max(0, l - 1);
@@ -67,7 +67,7 @@ struct suffix_array {
         int low = 1, high = n - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (compare(suffix[mid], t) < 0) {
+            if (compare(suf[mid], t) < 0) {
                 low = mid + 1;
             } else {
                 answer = mid;
@@ -82,7 +82,7 @@ struct suffix_array {
         int low = 1, high = n - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (compare(suffix[mid], t) <= 0) {
+            if (compare(suf[mid], t) <= 0) {
                 low = mid + 1;
             } else {
                 answer = mid;
@@ -94,7 +94,7 @@ struct suffix_array {
 
     int search(str &t) {
         int lower = lower_search(t);
-        if (compare(suffix[lower], t) != 0) return 0;
+        if (compare(suf[lower], t) != 0) return 0;
         int upper = upper_search(t);
         return upper - lower;
     }
