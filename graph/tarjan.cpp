4dd22da3ac46a3f3ef22d83692d512db381f60e6
thawan-xyz <thawan.xyz@gmail.com>
2025-08-04 21:02:47 -0300

    graph

---
+++ graph/tarjan.cpp
@@ -0,0 +1,32 @@
+array<array<int>> tarjan(array<array<int>> &graph) {
+    int n = graph.size() - 1, timer = 1;
+    array<array<int>> components;
+    array<int> id(n + 1), rank(n + 1);
+    stack<int> trail;
+    array<bool> inside(n + 1);
+
+    function<void(int)> dfs = [&](int a) -> void {
+        id[a] = rank[a] = timer++;
+        trail.push(a);
+        inside[a] = true;
+        for (int b : graph[a]) {
+            if (id[b] == 0) dfs(b);
+            if (inside[b]) rank[a] = min(rank[a], rank[b]);
+        }
+
+        if (id[a] == rank[a]) {
+            array<int> component;
+            int b = 0;
+            do {
+                b = trail.top();
+                trail.pop();
+                inside[b] = false;
+                component.push_back(b);
+            } while (b != a);
+            components.push_back(component);
+        }
+    };
+
+    for (int i = 1; i <= n; ++i) if (id[i] == 0) dfs(i);
+    return components;
+}
