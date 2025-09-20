d58c2bebe51fc03079593d3dc60a105e0a0cbb22
thawan-xyz <thawan.xyz@gmail.com>
2025-09-20 18:40:06 -0300

    math

---
+++ math/fast-fourier-transform.cpp
@@ -0,0 +1,86 @@
+int log2(int n) {
+    int c = 0;
+    while (n != 1) {
+        n >>= 1;
+        c++;
+    }
+    return c;
+}
+
+int pow2(int n) {
+    int p = 1;
+    while (p < n) {
+        p <<= 1;
+    }
+    return p;
+}
+
+int reverse(int i, int log) {
+    int k = 0;
+    for (int j = 0; j < log; ++j) {
+        if (i & (1 << j)) {
+            k |= 1 << (log - (j + 1));
+        }
+    }
+    return k;
+}
+
+array<complex<float>> FFT(array<complex<float>> p, bool inverse) {
+    int n = p.size();
+    int log = log2(n);
+    for (int i = 0; i < n; ++i) {
+        int j = reverse(i, log);
+        if (i < j) {
+            swap(p[i], p[j]);
+        }
+    }
+
+    for (int l = 1; l < n; l <<= 1) {
+        float ang = M_PI / l * (inverse ? -1 : 1);
+        complex<float> step(cos(ang), sin(ang));
+
+        for (int i = 0; i < n; i += l << 1) {
+            complex<float> w(1, 0);
+            for (int j = 0; j < l; ++j) {
+                complex<float> x = p[i + j];
+                complex<float> y = w * p[(i + j) + l];
+                p[i + j] = x + y;
+                p[(i + j) + l] = x - y;
+                w *= step;
+            }
+        }
+    }
+
+    if (inverse) {
+        for (complex<float> &z : p) {
+            z /= n;
+        }
+    }
+    return p;
+}
+
+array<int> multiply(array<int> &a, array<int> &b) {
+    int n = a.size() + b.size() - 1;
+    int m = pow2(n);
+
+    array<complex<float>> x(a.begin(), a.end());
+    array<complex<float>> y(b.begin(), b.end());
+    x.resize(m);
+    y.resize(m);
+
+    FFT(x, false);
+    FFT(y, false);
+
+    array<complex<float>> z(m);
+    for (int i = 0; i < m; ++i) {
+        z[i] = x[i] * y[i];
+    }
+
+    FFT(z, true);
+
+    array<int> c(n);
+    for (int i = 0; i < n; ++i) {
+        c[i] = round(z[i].real());
+    }
+    return c;
+}
