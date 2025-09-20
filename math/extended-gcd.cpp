57e17bff15d54b11b2e26915061a383d7390f3f2
thawan-xyz <thawan.xyz@gmail.com>
2025-09-20 19:10:21 -0300

    math

--- math/extended-gcd.cpp
+++ math/extended-gcd.cpp
@@ -1,6 +1,6 @@
-tuple<int, int, int> egcd(int a, int b) {
+tuple<int, int, int> extended_gcd(int a, int b) {
     if (b == 0) return {a, 1, 0};
 
-    auto [g, x, y] = egcd(b, a % b);
+    auto [g, x, y] = extended_gcd(b, a % b);
     return {g, y, x - (a / b) * y};
 }
