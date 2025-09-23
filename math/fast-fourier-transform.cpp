3090dfb2006e7e5cd8eca427085bd89ea71757e9
thawan-xyz <thawan.xyz@gmail.com>
2025-09-23 17:10:59 -0300

    math

--- math/fast-fourier-transform.cpp
+++ math/fast-fourier-transform.cpp
@@ -1,26 +1,31 @@
-int reverse(int i, int log) {
-    int k = 0;
-    for (int j = 0; j < log; ++j) {
-        if (i & (1 << j)) {
-            k |= 1 << (log - (j + 1));
+array<int> build(int n) {
+    array<int> order(n);
+    int l = __builtin_ctzll(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            if (i & (1 << j)) {
+                order[i] |= 1 << (l - (j + 1));
+            }
         }
     }
-    return k;
+    return order;
 }
 
-void FFT(array<complex<float>> &p, bool inverse) {
+void permute(array<complex<float>> &p, array<int> &order) {
     int n = p.size();
-    int log = log2(n);
     for (int i = 0; i < n; ++i) {
-        int j = reverse(i, log);
-        if (i < j) {
-            swap(p[i], p[j]);
-        }
+        int j = order[i];
+        if (i < j) swap(p[i], p[j]);
     }
+}
+
+void fast_fourier_transform(array<complex<float>> &p, int sign, array<int> &order) {
+    int n = p.size();
+    permute(p, order);
 
     for (int l = 1; l < n; l <<= 1) {
-        float ang = M_PI / l * (inverse ? -1 : 1);
-        complex<float> step(cos(ang), sin(ang));
+        float a = sign * M_PI / l;
+        complex<float> step(cos(a), sin(a));
 
         for (int i = 0; i < n; i += l << 1) {
             complex<float> w(1, 0);
@@ -34,35 +39,24 @@ void FFT(array<complex<float>> &p, bool inverse) {
         }
     }
 
-    if (inverse) {
-        for (complex<float> &z : p) {
-            z /= n;
-        }
-    }
+    if (sign == -1) for (complex<float> &c : p) c /= n;
 }
 
-array<int> multiply(array<int> &a, array<int> &b) {
+array<int> convolution(array<int> &a, array<int> &b) {
     int n = a.size() + b.size() - 1;
-    int m = pow2(n);
+    int m = 1 << (64 - __builtin_clzll(n - 1));
+    array<int> order = build(m);
 
-    array<complex<float>> x(a.begin(), a.end());
-    array<complex<float>> y(b.begin(), b.end());
-    x.resize(m);
-    y.resize(m);
-
-    FFT(x, false);
-    FFT(y, false);
+    array<complex<float>> x(a.begin(), a.end()); x.resize(m);
+    array<complex<float>> y(b.begin(), b.end()); y.resize(m);
+    fast_fourier_transform(x, 1, order);
+    fast_fourier_transform(y, 1, order);
 
     array<complex<float>> z(m);
-    for (int i = 0; i < m; ++i) {
-        z[i] = x[i] * y[i];
-    }
-
-    FFT(z, true);
+    for (int i = 0; i < m; ++i) z[i] = x[i] * y[i];
+    fast_fourier_transform(z, -1, order);
 
     array<int> c(n);
-    for (int i = 0; i < n; ++i) {
-        c[i] = round(z[i].real());
-    }
+    for (int i = 0; i < n; ++i) c[i] = round(z[i].real());
     return c;
 }
