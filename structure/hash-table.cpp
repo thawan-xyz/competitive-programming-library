2fbb6c1a1ead152b87cee92f9d1343fd0ed6ca12
thawan-xyz <thawan.xyz@gmail.com>
2025-09-28 11:47:20 -0300

    structure

--- structure/hash-table.cpp
+++ structure/hash-table.cpp
@@ -1,13 +1,13 @@
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 
-struct random_hash {
-    const int random = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct random {
+    const int seed = chrono::steady_clock::now().time_since_epoch().count();
 
     int operator()(int n) const {
-        return n ^ random;
+        return n ^ seed;
     }
 };
 
 template <typename K, typename V>
-using hash_table = gp_hash_table<K, V, random_hash>;
+using hash_table = gp_hash_table<K, V, random>;
