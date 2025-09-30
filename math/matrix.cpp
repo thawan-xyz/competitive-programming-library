9fed3c2b8c72b54da814fbbf3050a428ce17e95d
thawan-xyz <thawan.xyz@gmail.com>
2025-09-30 17:46:43 -0300

    math

---
+++ math/matrix.cpp
@@ -0,0 +1,33 @@
+using matrix = array<array<int>>;
+
+matrix product(matrix &a, matrix &b) {
+    int n = a.size();
+    int m = b[0].size();
+    int s = a[0].size();
+
+    matrix c(n, array<int>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            for (int k = 0; k < s; ++k) {
+                c[i][j] += (a[i][k] * b[k][j]) % mod;
+                c[i][j] %= mod;
+            }
+        }
+    }
+    return c;
+}
+
+matrix power(matrix &base, int exponent) {
+    int n = base.size();
+
+    matrix result(n, array<int>(n));
+    for (int i = 0; i < n; ++i) result[i][i] = 1;
+
+    while (exponent) {
+        if (exponent & 1) result = product(base, result);
+
+        base = product(base, base);
+        exponent >>= 1;
+    }
+    return result;
+}
