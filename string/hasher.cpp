5f5fe287c65f15ef92dcd5d39ac7f33949002f0b
thawan-xyz <thawan.xyz@gmail.com>
2025-11-12 20:22:06 -0300

    string

--- string/hasher.cpp
+++ string/hasher.cpp
@@ -1,28 +1,55 @@
 struct hasher {
     int n;
-    static const int p1 = 41, p2 = 53;
-    static const int m1 = 1e9 + 7, m2 = 1e9 + 9;
-    list<int> hash1, hash2;
-    list<int> pow1, pow2;
+    int a = 41, b = 53;
+    int m1 = 1e9 + 7, m2 = 1e9 + 9;
+    array<int> p1, p2;
+    array<int> h1, h2;
+    array<int> r1, r2;
 
-    hasher(str &s): n(s.length()), hash1(n + 1), hash2(n + 1), pow1(n + 1, 1), pow2(n + 1, 1) {
+    hasher(str &s): n(s.length()), p1(n + 1), p2(n + 1), h1(n), h2(n), r1(n), r2(n) {
+        p1[0] = 1;
+        p2[0] = 1;
         for (int i = 1; i <= n; ++i) {
-            hash1[i] = (p1 * hash1[i - 1] + s[i - 1]) % m1;
-            hash2[i] = (p2 * hash2[i - 1] + s[i - 1]) % m2;
-            
-            pow1[i] = (p1 * pow1[i - 1]) % m1;
-            pow2[i] = (p2 * pow2[i - 1]) % m2;
+            p1[i] = (a * p1[i - 1]) % m1;
+            p2[i] = (b * p2[i - 1]) % m2;
+        }
+
+        h1[0] = s[0];
+        h2[0] = s[0];
+        for (int i = 1; i < n; ++i) {
+            h1[i] = (a * h1[i - 1] + s[i]) % m1;
+            h2[i] = (b * h2[i - 1] + s[i]) % m2;
+        }
+
+        r1[n - 1] = s[n - 1];
+        r2[n - 1] = s[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            r1[i] = (a * r1[i + 1] + s[i]) % m1;
+            r2[i] = (b * r2[i + 1] + s[i]) % m2;
         }
     }
 
-    pair<int, int> range(int i, int j) {
-        int length = j - i + 1;
-        
-        int range1 = hash1[j + 1] - (hash1[i] * pow1[length]) % m1;
-        range1 %= m1; if (range1 < 0) range1 += m1;
-        int range2 = hash2[j + 1] - (hash2[i] * pow2[length]) % m2;
-        range2 %= m2; if (range2 < 0) range2 += m2;
+    int hash(int l, int r) {
+        int x = h1[r];
+        if (l > 0) x = (h1[r] - h1[l - 1] * p1[r - l + 1]) % m1;
+        if (x < 0) x += m1;
 
-        return {range1, range2};
+        int y = h2[r];
+        if (l > 0) y = (h2[r] - h2[l - 1] * p2[r - l + 1]) % m2;
+        if (y < 0) y += m2;
+
+        return (x << 32) | y;
+    }
+
+    int reverse(int l, int r) {
+        int x = r1[l];
+        if (r + 1 < n) x = (r1[l] - r1[r + 1] * p1[r - l + 1]) % m1;
+        if (x < 0) x += m1;
+
+        int y = r2[l];
+        if (r + 1 < n) y = (r2[l] - r2[r + 1] * p2[r - l + 1]) % m2;
+        if (y < 0) y += m2;
+
+        return (x << 32) | y;
     }
 };
