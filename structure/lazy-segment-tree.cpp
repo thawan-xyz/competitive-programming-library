de9ee3e305213d382fb70e55a3f6893faa689200
thawan-xyz <thawan.xyz@gmail.com>
2025-11-21 00:39:46 -0300

    structure

--- structure/lazy-segment-tree.cpp
+++ structure/lazy-segment-tree.cpp
@@ -1,26 +1,23 @@
-template <typename T>
 struct lazy_segment_tree {
     int size;
-    list<T> tree;
-    list<T> lazy;
+    array<T> tree;
+    array<T> lazy;
     T neutral, empty;
     F combine;
     G apply;
 
-    lazy_segment_tree(list<T> &base): size(base.size()), tree(4 * size), lazy(4 * size, empty) {
-        build(base);
-    }
+    lazy_segment_tree(int n): size(n), tree(4 * size), lazy(4 * size, empty) {}
 
-    void build(list<T> &base, int node = 0, int left = 0, int right = -1) {
+    void build(array<T> &base, int node = 1, int left = 0, int right = -1) {
         if (right == -1) right = size - 1;
 
         if (left == right) {
             tree[node] = base[left];
         } else {
             int middle = (left + right) / 2;
-            build(base, (2 * node) + 1, left, middle);
-            build(base, (2 * node) + 2, middle + 1, right);
-            tree[node] = combine(tree[(2 * node) + 1], tree[(2 * node) + 2]);
+            build(base, node << 1, left, middle);
+            build(base, (node << 1) | 1, middle + 1, right);
+            tree[node] = combine(tree[node << 1], tree[(node << 1) | 1]);
         }
     }
 
@@ -28,14 +25,14 @@ struct lazy_segment_tree {
         if (lazy[node] != empty) {
             tree[node] = apply(tree[node], lazy[node], (right - left) + 1);
             if (left != right) {
-                lazy[(2 * node) + 1] = combine(lazy[node], lazy[(2 * node) + 1]);
-                lazy[(2 * node) + 2] = combine(lazy[node], lazy[(2 * node) + 2]);
+                lazy[node << 1] = combine(lazy[node], lazy[node << 1]);
+                lazy[(node << 1) | 1] = combine(lazy[node], lazy[(node << 1) | 1]);
             }
             lazy[node] = empty;
         }
     }
 
-    void update(int i, int j, T value, int node = 0, int left = 0, int right = -1) {
+    void update(int i, int j, T value, int node = 1, int left = 0, int right = -1) {
         if (right == -1) right = size - 1;
 
         propagate(node, left, right);
@@ -44,18 +41,18 @@ struct lazy_segment_tree {
         } else {
             int middle = (left + right) / 2;
             if (i <= middle) {
-                update(i, j, value, (2 * node) + 1, left, middle);
+                update(i, j, value, node << 1, left, middle);
             }
             if (j > middle) {
-                update(i, j, value, (2 * node) + 2, middle + 1, right);
+                update(i, j, value, (node << 1) | 1, middle + 1, right);
             }
-            propagate((2 * node) + 1, left, middle);
-            propagate((2 * node) + 2, middle + 1, right);
-            tree[node] = combine(tree[(2 * node) + 1], tree[(2 * node) + 2]);
+            propagate(node << 1, left, middle);
+            propagate((node << 1) | 1, middle + 1, right);
+            tree[node] = combine(tree[node << 1], tree[(node << 1) | 1]);
         }
     }
 
-    T query(int i, int j, int node = 0, int left = 0, int right = -1) {
+    T query(int i, int j, int node = 1, int left = 0, int right = -1) {
         if (right == -1) right = size - 1;
 
         propagate(node, left, right);
@@ -65,10 +62,10 @@ struct lazy_segment_tree {
             T answer = neutral;
             int middle = (left + right) / 2;
             if (i <= middle) {
-                answer = combine(answer, query(i, j, (2 * node) + 1, left, middle));
+                answer = combine(answer, query(i, j, node << 1, left, middle));
             }
             if (j > middle) {
-                answer = combine(answer, query(i, j, (2 * node) + 2, middle + 1, right));
+                answer = combine(answer, query(i, j, (node << 1) | 1, middle + 1, right));
             }
             return answer;
         }
--- structure/segment-tree.cpp
+++ structure/segment-tree.cpp
@@ -1,28 +1,25 @@
-template <typename T>
 struct segment_tree {
     int size;
-    list<T> tree;
+    array<T> tree;
     T neutral;
     F combine;
 
-    segment_tree(list<T> &base): size(base.size()), tree(4 * size) {
-        build(base);
-    }
+    segment_tree(int n): size(n), tree(4 * size) {}
 
-    void build(list<T> &base, int node = 0, int left = 0, int right = -1) {
+    void build(array<T> &base, int node = 1, int left = 0, int right = -1) {
         if (right == -1) right = size - 1;
 
         if (left == right) {
             tree[node] = base[left];
         } else {
             int middle = (left + right) / 2;
-            build(base, (2 * node) + 1, left, middle);
-            build(base, (2 * node) + 2, middle + 1, right);
-            tree[node] = combine(tree[(2 * node) + 1], tree[(2 * node) + 2]);
+            build(base, node << 1, left, middle);
+            build(base, (node << 1) | 1, middle + 1, right);
+            tree[node] = combine(tree[node << 1], tree[(node << 1) | 1]);
         }
     }
 
-    void update(int i, T value, int node = 0, int left = 0, int right = -1) {
+    void update(int i, T value, int node = 1, int left = 0, int right = -1) {
         if (right == -1) right = size - 1;
 
         if (left == right) {
@@ -30,15 +27,15 @@ struct segment_tree {
         } else {
             int middle = (left + right) / 2;
             if (i <= middle) {
-                update(i, value, (2 * node) + 1, left, middle);
+                update(i, value, node << 1, left, middle);
             } else {
-                update(i, value, (2 * node) + 2, middle + 1, right);
+                update(i, value, (node << 1) | 1, middle + 1, right);
             }
-            tree[node] = combine(tree[(2 * node) + 1], tree[(2 * node) + 2]);
+            tree[node] = combine(tree[node << 1], tree[(node << 1) | 1]);
         }
     }
 
-    T query(int i, int j, int node = 0, int left = 0, int right = -1) {
+    T query(int i, int j, int node = 1, int left = 0, int right = -1) {
         if (right == -1) right = size - 1;
 
         if (i <= left and j >= right) {
@@ -47,10 +44,10 @@ struct segment_tree {
             T answer = neutral;
             int middle = (left + right) / 2;
             if (i <= middle) {
-                answer = combine(answer, query(i, j, (2 * node) + 1, left, middle));
+                answer = combine(answer, query(i, j, node << 1, left, middle));
             }
             if (j > middle) {
-                answer = combine(answer, query(i, j, (2 * node) + 2, middle + 1, right));
+                answer = combine(answer, query(i, j, (node << 1) | 1, middle + 1, right));
             }
             return answer;
         }
