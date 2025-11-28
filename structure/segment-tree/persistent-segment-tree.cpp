45ab5c59a68da33c00f66452bf34c4a23402d2d4
Thawan <thawan.xyz@gmail.com>
2025-11-28 15:34:46 -0300

    structure

--- structure/segment-tree/persistent-segment-tree.cpp
+++ structure/segment-tree/persistent-segment-tree.cpp
@@ -1,18 +1,17 @@
 struct persistent_segment_tree {
 private:
     struct node {
-        int v;
-        int l, r;
+        int v, l, r;
 
-        node(int v, int l, int r): v(v), l(l), r(r) {}
+        node(int v, int l = 0, int r = 0): v(v), l(l), r(r) {}
     };
 
     int n;
     array<node> t;
 
-    int leaf(int v) {
+    int terminal(int v) {
         int i = t.size();
-        t.push_back(node(v, 0, 0));
+        t.push_back(node(v));
         return i;
     }
 
@@ -23,14 +22,14 @@ private:
     }
 
     int build(array<int> &a, int l, int r) {
-        if (l == r) return leaf(a[l]);
+        if (l == r) return terminal(a[l]);
 
         int m = (l + r) / 2;
         return internal(build(a, l, m), build(a, m + 1, r));
     }
 
     int update(int i, int v, int p, int l, int r) {
-        if (l == r) return leaf(t[p].v + v);
+        if (l == r) return terminal(t[p].v + v);
 
         int m = (l + r) / 2;
         if (i <= m) return internal(update(i, v, t[p].l, l, m), t[p].r);
@@ -38,8 +37,7 @@ private:
     }
 
     int query(int ql, int qr, int p, int l, int r) {
-        if (not p) return 0;
-        if (ql > r or qr < l) return 0;
+        if (not p or (ql > r or qr < l)) return 0;
         if (ql <= l and qr >= r) return t[p].v;
 
         int m = (l + r) / 2;
@@ -55,26 +53,26 @@ private:
     }
 
 public:
-    array<int> r;
+    array<int> x;
 
     persistent_segment_tree(int n): n(n), t(1) {
-        r.reserve(n);
+        x.reserve(n);
         t.reserve(8 * n);
     }
 
     void build(array<int> &a) {
-        r.push_back(build(a, 0, n - 1));
+        x.push_back(build(a, 0, n - 1));
     }
 
     void update(int i, int v, int p) {
-        r.push_back(update(i, v, p, 0, n - 1));
+        x.push_back(update(i, v, p, 0, n - 1));
     }
 
-    int query(int ql, int qr, int p) {
-        return query(ql, qr, p, 0, n - 1);
+    int query(int l, int r, int p) {
+        return query(l, r, p, 0, n - 1);
     }
 
-    void copy(int ql, int qr, int p, int q) {
-        r.push_back(copy(ql, qr, p, q, 0, n - 1));
+    void copy(int l, int r, int p, int q) {
+        x.push_back(copy(l, r, p, q, 0, n - 1));
     }
 };
