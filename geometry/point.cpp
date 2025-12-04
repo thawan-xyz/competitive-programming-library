bb91bebd2d41151d0879e03bf9ec2a7fa9ff9245
Thawan <thawan.xyz@gmail.com>
2025-12-04 20:41:23 -0300

    geometry

--- geometry/point.cpp
+++ geometry/point.cpp
@@ -6,6 +6,24 @@ struct point {
     }
 };
 
+int orient(point &a, point &b, point &p) {
+    point v = b - a;
+    point w = p - a;
+
+    float cross = v.x * w.y - v.y * w.x;
+    if (cross > 0) return -1;
+    if (cross < 0) return +1;
+    return 0;
+}
+
+bool between(point &a, point &b, point &p) {
+    if (orient(a, b, p) != 0) return false;
+
+    bool x = min(a.x, b.x) <= p.x and p.x <= max(a.x, b.x);
+    bool y = min(a.y, b.y) <= p.y and p.y <= max(a.y, b.y)
+    return x and y;
+}
+
 point project(point &a, point &b, point &p) {
     point v = b - a;
     point w = p - a;
