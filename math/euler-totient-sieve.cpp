3d089422485bc60e52bff014f651ebb65fa57c1a
Thawan <thawan.xyz@gmail.com>
2025-12-14 15:25:52 -0300

    math

---
+++ math/euler-totient-sieve.cpp
@@ -0,0 +1,13 @@
+array<int> euler_totient_sieve(int n) {
+    array<int> phi(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        phi[i] = i;
+    }
+
+    for (int i = 2; i <= n; ++i) if (phi[i] == i) {
+        for (int j = i; j <= n; j += i) {
+            phi[j] -= phi[j] / i;
+        }
+    }
+    return phi;
+}
