c6f05c8cec6485e385d58cc2c3e116145212e79c
Thawan <thawan.xyz@gmail.com>
2025-12-15 20:01:08 -0300

    math

--- math/number-theory/mobius.cpp
+++ math/number-theory/mobius.cpp
@@ -1,14 +1,11 @@
 array<int> mobius(int n) {
-    array<int> µ(n + 1);
-    for (int i = 1; i <= n; ++i) {
-        µ[i] = 1;
-    }
-
-    array<bool> c(n + 1);
-    for (int i = 2; i <= n; ++i) if (not c[i]) {
-        for (int j = i; j <= n; j += i) {
+    array<int> µ(n + 1, 1);
+    array<bool> p(n + 1, true);
+    for (int i = 2; i <= n; ++i) if (p[i]) {
+        µ[i] = -1;
+        for (int j = 2 * i; j <= n; j += i) {
             µ[j] *= -1;
-            c[j] = true;
+            p[j] = false;
         }
         for (int j = i * i; j <= n; j += i * i) {
             µ[j] = 0;
