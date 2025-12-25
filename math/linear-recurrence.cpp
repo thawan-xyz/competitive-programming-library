ee57c6f9e166ea6236be781a25b877365c779e3a
Thawan <thawan.xyz@gmail.com>
2025-12-25 20:39:03 -0300

    math

--- math/linear-recurrence.cpp
+++ math/linear-recurrence.cpp
@@ -1,7 +1,41 @@
-int linear_recurrence(int n, int k, list<int> &c, list<int> &f) {
+using matrix = array<array<int>>;
+
+matrix product(matrix &a, matrix &b) {
+    int n = a.size();
+    int m = b[0].size();
+    int s = a[0].size();
+
+    matrix c(n, array<int>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            for (int k = 0; k < s; ++k) {
+                c[i][j] += (a[i][k] * b[k][j]) % mod;
+                c[i][j] %= mod;
+            }
+        }
+    }
+    return c;
+}
+
+matrix pow(matrix &b, int e) {
+    int n = b.size();
+
+    matrix a(n, array<int>(n));
+    for (int i = 0; i < n; ++i) a[i][i] = 1;
+
+    while (e) {
+        if (e & 1) a = product(a, b);
+
+        b = product(b, b);
+        e >>= 1;
+    }
+    return a;
+}
+
+int linear_recurrence(int n, int k, array<int> &c, array<int> &f) {
     if (n < k) return f[n];
 
-    matrix m(k, list<int>(k));
+    matrix m(k, array<int>(k));
     for (int i = 0; i < k; ++i) m[0][i] = c[i];
     for (int i = 1; i < k; ++i) m[i][i - 1] = 1;
 
@@ -9,16 +43,16 @@ int linear_recurrence(int n, int k, list<int> &c, list<int> &f) {
 
     int answer = 0;
     for (int i = 0; i < k; ++i) {
-        answer += (p[0][i] * f[i]) % mod;
+        answer += (p[0][i] * f[k - (i + 1)]) % mod;
         answer %= mod;
     }
     return answer;
 }
 
-int linear_recurrence_constant(int n, int k, list<int> &c, int constant, list<int> &f) {
+int linear_recurrence_constant(int n, int k, array<int> &c, int constant, array<int> &f) {
     if (n < k) return f[n];
 
-    matrix m(k + 1, list<int>(k + 1));
+    matrix m(k + 1, array<int>(k + 1));
     for (int i = 0; i < k; ++i) m[0][i] = c[i];
     m[0][k] = 1;
     for (int i = 1; i < k; ++i) m[i][i - 1] = 1;
@@ -28,7 +62,7 @@ int linear_recurrence_constant(int n, int k, list<int> &c, int constant, list<in
 
     int answer = 0;
     for (int i = 0; i < k; ++i) {
-        answer += (p[0][i] * f[i]) % mod;
+        answer += (p[0][i] * f[k - (i + 1)]) % mod;
         answer %= mod;
     }
     answer += (p[0][k] * constant) % mod;
