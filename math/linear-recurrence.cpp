1a9f1a313ba1082ad3154a22d038daa3fe6cb7c5
Thawan <thawan.xyz@gmail.com>
2026-01-07 22:34:34 -0300

    math

--- math/linear-recurrence.cpp
+++ math/linear-recurrence.cpp
@@ -1,37 +1,3 @@
-using matrix = array<array<int>>;
-
-matrix product(matrix &a, matrix &b) {
-    int n = a.size();
-    int m = b[0].size();
-    int s = a[0].size();
-
-    matrix c(n, array<int>(m));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            for (int k = 0; k < s; ++k) {
-                c[i][j] += (a[i][k] * b[k][j]) % mod;
-                c[i][j] %= mod;
-            }
-        }
-    }
-    return c;
-}
-
-matrix pow(matrix &b, int e) {
-    int n = b.size();
-
-    matrix a(n, array<int>(n));
-    for (int i = 0; i < n; ++i) a[i][i] = 1;
-
-    while (e) {
-        if (e & 1) a = product(a, b);
-
-        b = product(b, b);
-        e >>= 1;
-    }
-    return a;
-}
-
 int linear_recurrence(int n, int k, array<int> &c, array<int> &f) {
     if (n < k) return f[n];
 
--- math/pow.cpp
+++ math/pow.cpp
@@ -2,9 +2,9 @@ int pow(int b, int e, int mod) {
     int a = 1;
 
     while (e) {
-        if (e & 1) a = (a * b) % mod;
+        if (e & 1) a *= b, a %= mod;
 
-        b = (b * b) % mod;
+        b *= b, b %= mod;
         e >>= 1;
     }
     return a;
