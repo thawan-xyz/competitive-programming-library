0038c5e155e5e0f2dbe16719d09c5141cac86117
Thawan <thawan.xyz@gmail.com>
2026-01-16 14:00:34 -0300

    extra

--- extra/mo.cpp
+++ extra/mo.cpp
@@ -1,19 +1,32 @@
+int hilbert(int i, int j) {
+    static const array<int> d = {3, 0, 0, 1};
+
+    int h = 0, b = 1 << 20, r = 0;
+    while (b) {
+        int x = (i & b) != 0;
+        int y = (j & b) != 0;
+        int q = (((x << 1) | y) + r) & 3;
+
+        h = (h << 2) | q;
+        r = (r + d[q]) & 3;
+        b >>= 1;
+    }
+    return h;
+}
+
+struct query {
+    int l, r, k, h;
+
+    query(int l, int r, int k): l(l), r(r), k(k) {
+        h = hilbert(l, r);
+    }
+
+    bool operator<(const query &other) const {
+        return h < other.h;
+    }
+};
+
 struct mo {
-private:
-    struct query {
-        inline static int block_size;
-        int l, r, k, block;
-
-        query(int l, int r, int k): l(l), r(r), k(k) {
-            block = l / block_size;
-        }
-
-        bool operator<(const query &other) const {
-            if (block != other.block) return block < other.block;
-            return ((block & 1) == 0) ? r < other.r : r > other.r;
-        }
-    };
-
     int n, q, i, j, t;
     array<int> &a;
 
@@ -21,21 +34,18 @@ private:
 
     void remove(int k) {}
 
-public:
     array<int> answer;
     array<query> queries;
 
     mo(array<int> &a, int q): n(a.size()), q(q), i(0), j(-1), t(0), a(a) {
         answer.resize(q);
-
-        query::block_size = sqrt(n);
         queries.reserve(q);
     }
 
     void run() {
         sort(queries.begin(), queries.end());
 
-        for (auto &[l, r, k] : queries) {
+        for (auto &[l, r, k, _] : queries) {
             while (i > l) insert(--i);
             while (j < r) insert(++j);
             while (i < l) remove(i++);
