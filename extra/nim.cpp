35c2117babe02ad095b498191c4fe2402ab7b924
Thawan <thawan.xyz@gmail.com>
2026-01-28 07:44:12 -0300

    extra

--- extra/nim.cpp
+++ extra/nim.cpp
@@ -1,4 +1,4 @@
-int nim(list<int> &piles, list<int> &moves) {
+bool nim(list<int> &piles, list<int> &moves) {
     int n = *max(piles.begin(), piles.end());
     list<int> grundy(n + 1);
 
@@ -17,5 +17,29 @@ int nim(list<int> &piles, list<int> &moves) {
     for (int pile : piles) {
         answer ^= grundy[pile];
     }
-    return answer;
+    return answer != 0;
+}
+
+bool misere(list<int> &piles, list<int> &moves) {
+    int n = *max(piles.begin(), piles.end());
+    list<int> grundy(n + 1);
+
+    for (int i = 1; i <= n; ++i) {
+        set<int> reach;
+        for (int move : moves) if (move <= i) {
+            reach.insert(grundy[i - move]);
+        }
+
+        int mex = 0;
+        while (reach.contains(mex)) mex++;
+        grundy[i] = mex;
+    }
+
+    bool all_one = true;
+    int answer = 0;
+    for (int pile : piles) {
+        all_one &= grundy[pile] == 1;
+        answer ^= grundy[pile];
+    }
+    return all_one ? (piles.size() & 1) == 0 : answer != 0;
 }
