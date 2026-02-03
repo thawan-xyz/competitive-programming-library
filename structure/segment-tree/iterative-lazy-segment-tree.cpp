455a77ba7e60ec1b60b6967109ae7a1df8e0d7b7
Thawan <thawan.xyz@gmail.com>
2026-02-03 14:31:31 -0300

    structure

--- structure/segment-tree/iterative-lazy-segment-tree.cpp
+++ structure/segment-tree/iterative-lazy-segment-tree.cpp
@@ -1,6 +1,6 @@
 struct iterative_lazy_segment_tree {
     int n, h;
-    array<int> tree, lazy;
+    list<int> tree, lazy;
 
     iterative_lazy_segment_tree(int n): n(n), h(32 - __builtin_clz(n)), tree(2 * n), lazy(2 * n) {}
 
@@ -8,79 +8,62 @@ struct iterative_lazy_segment_tree {
         return a + b;
     }
 
-    int compose(int a, int b) {
-        return a + b;
+    int compose(int c, int p) {
+        if (p == 0) return c;
+        return c + p;
     }
 
     int update(int a, int x, int l) {
+        if (x == 0) return a;
         return a + x * l;
     }
 
-    void build(array<int> &a) {
-        for (int i = 0; i < n; ++i) {
-            tree[n + i] = a[i];
-        }
-        for (int i = n - 1; i > 0; --i) {
-            tree[i] = merge(tree[i << 1], tree[(i << 1) | 1]);
-        }
+    void build(list<int> &a) {
+        for (int i = 0; i < n; ++i) tree[n + i] = a[i];
+        for (int i = n - 1; i > 0; --i) tree[i] = merge(tree[i << 1], tree[(i << 1) | 1]);
     }
 
     void push(int i) {
         for (int k = h; k > 0; --k) {
-            int p = i >> k;
-            int l = 1 << (k - 1);
-            if (lazy[p] != 0) {
-                lazy[p << 1] = compose(lazy[p << 1], lazy[p]);
-                tree[p << 1] = update(tree[p << 1], lazy[p], l);
-                lazy[(p << 1) | 1] = compose(lazy[(p << 1) | 1], lazy[p]);
-                tree[(p << 1) | 1] = update(tree[(p << 1) | 1], lazy[p], l);
-                lazy[p] = 0;
+            int p = i >> k, l = 1 << (k - 1);
+            for (int c : {p << 1, (p << 1) | 1}) {
+                tree[c] = update(tree[c], lazy[p], l);
+                lazy[c] = compose(lazy[c], lazy[p]);
             }
+            lazy[p] = 0;
         }
     }
 
     void pull(int i) {
         for (int p = i >> 1, l = 2; p > 0; p >>= 1, l <<= 1) {
             tree[p] = merge(tree[p << 1], tree[(p << 1) | 1]);
-            if (lazy[p] != 0) {
-                tree[p] = update(tree[p], lazy[p], l);
-            }
+            tree[p] = update(tree[p], lazy[p], l);
         }
     }
 
     void modify(int i, int j, int x) {
-        push(i + n);
-        push(j + n);
+        push(i + n), push(j + n);
         for (int p = i + n, q = j + (n + 1), l = 1; p < q; p >>= 1, q >>= 1, l <<= 1) {
             if (p & 1) {
-                lazy[p] = compose(lazy[p], x);
                 tree[p] = update(tree[p], x, l);
+                lazy[p] = compose(lazy[p], x);
                 ++p;
             }
             if (q & 1) {
                 --q;
-                lazy[q] = compose(lazy[q], x);
                 tree[q] = update(tree[q], x, l);
+                lazy[q] = compose(lazy[q], x);
             }
         }
-        pull(i + n);
-        pull(j + n);
+        pull(i + n), pull(j + n);
     }
 
     int query(int i, int j) {
-        push(i + n);
-        push(j + n);
-        int l = 0;
-        int r = 0;
+        push(i + n), push(j + n);
+        int l = 0, r = 0;
         for (i += n, j += n + 1; i < j; i >>= 1, j >>= 1) {
-            if (i & 1) {
-                l = merge(l, tree[i]);
-                ++i;
-            }
-            if (j & 1) {
-                --j;
-                r = merge(tree[j], r);
-            }
+            if (i & 1) l = merge(l, tree[i++]);
+            if (j & 1) r = merge(tree[--j], r);
         }
         return merge(l, r);
     }
--- structure/segment-tree/iterative-segment-tree.cpp
+++ structure/segment-tree/iterative-segment-tree.cpp
@@ -9,34 +9,21 @@ struct iterative_segment_tree {
     }
 
     void build(list<int> &a) {
-        for (int i = 0; i < n; ++i) {
-            tree[n + i] = a[i];
-        }
-        for (int i = n - 1; i > 0; --i) {
-            tree[i] = merge(tree[i << 1], tree[(i << 1) | 1]);
-        }
+        for (int i = 0; i < n; ++i) tree[n + i] = a[i];
+        for (int i = n - 1; i > 0; --i) tree[i] = merge(tree[i << 1], tree[(i << 1) | 1]);
     }
 
     void modify(int i, int x) {
         i += n;
         tree[i] = x;
-        for (i >>= 1; i > 0; i >>= 1) {
-            tree[i] = merge(tree[i << 1], tree[(i << 1) | 1]);
-        }
+        for (i >>= 1; i > 0; i >>= 1) tree[i] = merge(tree[i << 1], tree[(i << 1) | 1]);
     }
 
     int query(int i, int j) {
-        int l = 0;
-        int r = 0;
+        int l = 0, r = 0;
         for (i += n, j += n + 1; i < j; i >>= 1, j >>= 1) {
-            if (i & 1) {
-                l = merge(l, tree[i]);
-                ++i;
-            }
-            if (j & 1) {
-                --j;
-                r = merge(tree[j], r);
-            }
+            if (i & 1) l = merge(l, tree[i++]);
+            if (j & 1) r = merge(tree[--j], r);
         }
         return merge(l, r);
     }
