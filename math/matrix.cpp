a928187130b1c20796347acb0e03c00c6a5660f6
Thawan <thawan.xyz@gmail.com>
2026-02-16 18:14:24 -0300

    math

--- math/matrix.cpp
+++ math/matrix.cpp
@@ -1,28 +1,32 @@
-using matrix = list<list<int>>;
+struct matrix : list<list<int>> {
+    matrix(int n, int m, int x = 0): list<list<int>>(n, list<int>(m, x)) {}
 
-matrix operator*(const matrix &a, const matrix &b) {
-    int n = a.size();
-    int m = b[0].size();
-    int h = b.size();
-    matrix r(n, list<int>(m));
-    for (int i = 0; i < n; ++i) {
-        for (int k = 0; k < h; ++k) {
-            for (int j = 0; j < m; ++j) {
-                r[i][j] = (r[i][j] + a[i][k] * b[k][j]) % mod;
+    matrix operator*(const matrix &b) const {
+        const matrix &a = *this;
+        int n = a.size();
+        int m = b[0].size();
+        int h = b.size();
+        matrix r(n, m);
+        for (int i = 0; i < n; ++i) {
+            for (int k = 0; k < h; ++k) {
+                for (int j = 0; j < m; ++j) {
+                    r[i][j] = (r[i][j] + a[i][k] * b[k][j]) % mod;
+                }
             }
         }
+        return r;
     }
-    return r;
-}
 
-matrix operator^(matrix a, int b) {
-    int n = a.size();
-    matrix r(n, list<int>(n));
-    for (int i = 0; i < n; ++i) r[i][i] = 1;
-    while (b != 0) {
-        if (b & 1) r = r * a;
-        a = a * a;
-        b >>= 1;
+    matrix operator^(int b) const {
+        matrix a = *this;
+        int n = a.size();
+        matrix r(n, n);
+        for (int i = 0; i < n; ++i) r[i][i] = 1;
+        while (b != 0) {
+            if (b & 1) r = r * a;
+            a = a * a;
+            b >>= 1;
+        }
+        return r;
     }
-    return r;
-}
+};
