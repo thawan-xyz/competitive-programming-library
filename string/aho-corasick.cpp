bfb3bc4fabd57eea62378b2eab661f17aeafcc16
Thawan <thawan.xyz@gmail.com>
2026-02-16 21:54:59 -0300

    string

--- string/aho-corasick.cpp
+++ string/aho-corasick.cpp
@@ -1,27 +1,28 @@
 struct aho_corasick {
 private:
     struct node {
+        int fail = 0;
+        int exit = 0;
         array<int, 26> next = {};
-        int fail;
         list<int> end;
-
-        node(): fail(0), end() {}
     };
 
     list<node> trie;
 
 public:
-    aho_corasick(): trie(1) {}
+    aho_corasick(int n = 0) {
+        trie.reserve(n + 5);
+        trie.emplace_back();
+    }
 
     void insert(str &s) {
         int i = 0;
         for (char &c : s) {
-            int x = c - 'a';
-            if (not trie[i].next[x]) {
-                trie[i].next[x] = trie.size();
-                trie.push_back(node());
+            if (trie[i].next[c - 'a'] == 0) {
+                trie[i].next[c - 'a'] = trie.size();
+                trie.emplace_back();
             }
-            i = trie[i].next[x];
+            i = trie[i].next[c - 'a'];
         }
         trie[i].end.push_back(s.size());
     }
@@ -30,17 +31,22 @@ public:
         queue<int> q;
         for (int c = 0; c < 26; ++c) {
             int i = trie[0].next[c];
-            if (i) {
+            if (i != 0) {
                 q.push(i);
             }
         }
         while (q.size()) {
             int a = q.front(); q.pop();
+            int f = trie[a].fail;
+            if (trie[f].end.empty()) {
+                trie[a].exit = trie[f].exit;
+            } else {
+                trie[a].exit = f;
+            }
             for (int c = 0; c < 26; ++c) {
                 int b = trie[a].next[c];
-                if (b) {
+                if (b != 0) {
                     trie[b].fail = trie[trie[a].fail].next[c];
-                    trie[b].end += trie[trie[b].fail].end;
                     q.push(b);
                 } else {
                     trie[a].next[c] = trie[trie[a].fail].next[c];
@@ -50,15 +56,16 @@ public:
     }
 
     list<int> match(str &t) {
-        list<int> a;
+        list<int> p;
         int n = t.length();
         for (int i = 0, k = 0; k < n; ++k) {
-            int x = t[k] - 'a';
-            i = trie[i].next[x];
-            for (int &l : trie[i].end) {
-                a.push_back((k - l) + 1);
+            i = trie[i].next[t[k] - 'a'];
+            for (int j = i; j != 0; j = trie[j].exit) {
+                for (int l : trie[j].end) {
+                    p.push_back(k - l + 1);
+                }
             }
         }
-        return a;
+        return p;
     }
 };
