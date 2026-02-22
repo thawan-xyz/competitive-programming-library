f347a6c1740a8e987d566a74c5f7e27c5e4ffb75
Thawan <thawan.xyz@gmail.com>
2026-02-22 14:06:09 -0300

    string

--- string/palindromic-tree.cpp
+++ string/palindromic-tree.cpp
@@ -1,9 +1,9 @@
 struct palindromic_tree {
     struct node {
-        int length, link;
+        int length, fail;
         array<int, 26> child = {};
 
-        node(int length): length(length), link(0) {}
+        node(int length): length(length), fail(0) {}
     };
 
     int suffix;
@@ -16,11 +16,13 @@ struct palindromic_tree {
         tree.reserve(n + 5);
         tree.push_back(node(0));
         tree.push_back(node(-1));
-        tree[0].link = 1;
+        tree[0].fail = 1;
     }
 
     int follow(int id) {
-        while (text[(text.size() - tree[id].length) - 2] != text.back()) id = tree[id].link;
+        while (text[(text.size() - tree[id].length) - 2] != text.back()) {
+            id = tree[id].fail;
+        }
         return id;
     }
 
@@ -32,7 +34,7 @@ struct palindromic_tree {
             int id = tree.size();
             tree.push_back(node(tree[suffix].length + 2));
 
-            tree[id].link = tree[follow(tree[suffix].link)].child[c - 'a'];
+            tree[id].fail = tree[follow(tree[suffix].fail)].child[c - 'a'];
             tree[suffix].child[c - 'a'] = id;
         }
         suffix = tree[suffix].child[c - 'a'];
