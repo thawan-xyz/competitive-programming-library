7ca66c0729ef06edbe81a3d661bbda2c008042d5
Thawan <thawan.xyz@gmail.com>
2026-02-22 14:40:00 -0300

    graph

--- graph/tarjan.cpp
+++ graph/tarjan.cpp
@@ -1,4 +1,4 @@
-list<list<int>> tarjan(list<list<int>> &g, bool one) {
+list<list<int>> tarjan(list<list<int>> &g, bool one_based) {
     int n = g.size(), timer = 1;
     list<list<int>> components;
     list<int> id(n), low(n);
@@ -30,12 +30,38 @@ list<list<int>> tarjan(list<list<int>> &g, bool one) {
         }
     };
 
-    for (int i = one; i < n; ++i) {
-        if (id[i] == 0) {
-            dfs(dfs, i);
+    for (int a = one_based; a < n; ++a) {
+        if (id[a] == 0) {
+            dfs(dfs, a);
         }
     }
 
     reverse(components.begin(), components.end());
     return components;
+}
+
+pair<list<list<int>>, list<int>> condensed_graph(list<list<int>> &g, bool one_based) {
+    int n = g.size();
+    list<list<int>> components = tarjan(g, one_based);
+    int m = components.size();
+
+    list<int> id(n);
+    for (int i = 0; i < m; ++i) {
+        for (int a : components[i]) {
+            id[a] = i;
+        }
+    }
+
+    list<list<int>> c(m);
+    for (int a = one_based; a < n; ++a) {
+        for (int b : g[a]) if (id[a] != id[b]) {
+            c[id[a]].push_back(id[b]);
+        }
+    }
+
+    for (int i = 0; i < m; ++i) {
+        sort(c[i].begin(), c[i].end());
+        c[i].erase(unique(c[i].begin(), c[i].end()), c[i].end());
+    }
+    return {c, id};
 }
