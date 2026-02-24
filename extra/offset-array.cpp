a03d0a042af5ddf569214d40038820d77a8b2574
Thawan <thawan.xyz@gmail.com>
2026-02-23 21:54:17 -0300

    extra

--- extra/offset-array.cpp
+++ extra/offset-array.cpp
@@ -1,8 +1,9 @@
+template <typename T>
 struct offset_array {
     int l;
-    list<int> a;
+    list<T> a;
 
-    offset_array(int l, int r, int x = 0): l(l), a((r - l) + 1, x) {}
+    offset_array(int l, int r, T x = T()): l(l), a((r - l) + 1, x) {}
 
     int &operator[](int i) {
         return a[i - l];
