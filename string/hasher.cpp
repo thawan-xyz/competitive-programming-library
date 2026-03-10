f9cb75d61caa4894c5f6aa11141310148383e6f2
thawan-xyz <thawan.xyz@gmail.com>
2026-03-10 10:07:12 -0300

    string

--- string/hasher.cpp
+++ string/hasher.cpp
@@ -1,53 +1,34 @@
-struct hasher {
-    static constexpr array<int, 2> base = {41, 53};
-    static constexpr array<int, 2> mod = {1000000007, 1000000009};
+const array<int, 2> base = {41, 53};
+const array<int, 2> mod = {1000000007, 1000000009};
 
+struct hasher {
     int n;
     vector<array<int, 2>> pow;
     vector<array<int, 2>> pref, suf;
 
     hasher(const string &s): n(s.length()), pow(n + 1), pref(n + 1), suf(n + 2) {
-        pow[0][0] = pow[0][1] = 1;
-        for (int i = 1; i <= n; ++i) {
-            pow[i][0] = (pow[i - 1][0] * base[0]) % mod[0];
-            pow[i][1] = (pow[i - 1][1] * base[1]) % mod[1];
-        }
-        pref[0][0] = pref[0][1] = 0;
-        for (int i = 1; i <= n; ++i) {
-            pref[i][0] = (pref[i - 1][0] * base[0] + s[i - 1]) % mod[0];
-            pref[i][1] = (pref[i - 1][1] * base[1] + s[i - 1]) % mod[1];
-        }
-        suf[n + 1][0] = suf[n + 1][1] = 0;
-        for (int i = n; i >= 1; --i) {
-            suf[i][0] = (suf[i + 1][0] * base[0] + s[i - 1]) % mod[0];
-            suf[i][1] = (suf[i + 1][1] * base[1] + s[i - 1]) % mod[1];
+        pow[0] = {1, 1};
+        for (int i = 0; i < n; ++i) for (int j : {0, 1}) {
+            pow[i + 1][j] = (pow[i][j] * base[j]) % mod[j];
+            pref[i + 1][j] = (pref[i][j] * base[j] + s[i]) % mod[j];
+            suf[n - i][j] = (suf[n - i + 1][j] * base[j] + s[n - i - 1]) % mod[j];
         }
     }
 
-    array<int, 2> f_hash(int l, int r) {
-        ++l, ++r;
-        int len = r - l + 1;
-        array<int, 2> res;
-        res[0] = (pref[r][0] - (pref[l - 1][0] * pow[len][0])) % mod[0];
-        if (res[0] < 0) res[0] += mod[0];
-        res[1] = (pref[r][1] - (pref[l - 1][1] * pow[len][1])) % mod[1];
-        if (res[1] < 0) res[1] += mod[1];
-        return res;
+    int get(vector<array<int, 2>> &h, int l, int r) {
+        array<int, 2> a;
+        for (int i : {0, 1}) {
+            a[i] = (h[r][i] - h[l][i] * pow[abs(r - l)][i]) % mod[i];
+            if (a[i] < 0) a[i] += mod[i];
+        }
+        return (a[0] << 32) | a[1];
     }
 
-    array<int, 2> r_hash(int l, int r) {
-        ++l, ++r;
-        int len = r - l + 1;
-        array<int, 2> res;
-        res[0] = (suf[l][0] - (suf[r + 1][0] * pow[len][0])) % mod[0];
-        if (res[0] < 0) res[0] += mod[0];
-        res[1] = (suf[l][1] - (suf[r + 1][1] * pow[len][1])) % mod[1];
-        if (res[1] < 0) res[1] += mod[1];
-        return res;
+    int normal(int l, int r) {
+        return get(pref, l, r + 1);
     }
 
-    bool palindrome(int l, int r) {
-        if (l >= r) return true;
-        return f_hash(l, r) == r_hash(l, r);
+    int reverse(int l, int r) {
+        return get(suf, r + 2, l + 1);
     }
 };
