726b5685f8c8b53a38a430f8b4cbf18611ffb01d
Thawan <thawan.xyz@gmail.com>
2026-03-10 18:15:05 -0300

    square-root

--- square-root/mo.cpp
+++ square-root/mo.cpp
@@ -1,51 +1,29 @@
-int hilbert(int i, int j) {
-    static const array<int, 4> d = {3, 0, 0, 1};
-
-    int h = 0, b = 1 << 20, r = 0;
-    while (b != 0) {
-        int x = (i & b) != 0;
-        int y = (j & b) != 0;
-        int q = (((x << 1) | y) + r) & 3;
-
-        h = (h << 2) | q;
-        r = (r + d[q]) & 3;
-        b >>= 1;
+int hilbert(int l, int r) {
+    static array<int, 4> d = {3, 0, 0, 1};
+    int h = 0, c = 0;
+    for (int s = 20; s >= 0; --s) {
+        int q = (((l >> s & 1) << 1 | (r >> s & 1)) + c) & 3;
+        h = h << 2 | q;
+        c = (c + d[q]) & 3;
     }
     return h;
 }
 
-struct query {
-    int l, r, i, h;
+vector<int> mo(vector<int> &a, vector<array<int, 4>> &q) {
+    for (auto &[h, l, r, id] : q) h = hilbert(l, r);
+    sort(q.begin(), q.end());
 
-    query(int l, int r, int i): l(l), r(r), i(i) {
-        h = hilbert(l, r);
+    auto insert = [&](int x) -> void {};
+    auto remove = [&](int x) -> void {};
+
+    vector<int> ans(q.size());
+    int t = 0, i = 0, j = -1;
+    for (auto &[h, l, r, id] : q) {
+        while (i > l) insert(a[--i]);
+        while (j < r) insert(a[++j]);
+        while (i < l) remove(a[i++]);
+        while (j > r) remove(a[j--]);
+        ans[id] = t;
     }
-
-    bool operator<(const query &other) const {
-        return h < other.h;
-    }
-};
-
-struct mo {
-    int t = 0;
-
-    void insert(int x) {}
-
-    void remove(int x) {}
-
-    vector<int> run(vector<int> &a, vector<query> &q) {
-        vector<int> answer(q.size());
-        sort(q.begin(), q.end());
-
-        int i = 0, j = -1;
-        for (auto &[l, r, k, h] : q) {
-            while (i > l) insert(a[--i]);
-            while (j < r) insert(a[++j]);
-            while (i < l) remove(a[i++]);
-            while (j > r) remove(a[j--]);
-
-            answer[k] = t;
-        }
-        return answer;
-    }
-};
+    return ans;
+}
