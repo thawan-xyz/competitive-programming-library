3760d54e169a06307d66404194271ffb8f176fe9
Thawan <thawan.xyz@gmail.com>
2026-03-13 13:45:21 -0300

    structure

--- structure/sparse-table.cpp
+++ structure/sparse-table.cpp
@@ -2,33 +2,21 @@ struct sparse_table {
     int n;
     vector<vector<int>> table;
 
-    static int log2(int x) {
-        return __lg(x);
-    }
-
-    static int pow2(int x) {
-        return 1 << x;
-    }
-
-    sparse_table(int n): n(n), table(n, vector<int>(log2(n) + 1)) {}
-
     int merge(int x, int y) {
         return gcd(x, y);
     }
 
-    void build(vector<int> &a) {
-        for (int i = 0; i < n; ++i) {
-            table[i][0] = a[i];
-        }
-        for (int k = 1; k <= log2(n); ++k) {
-            for (int i = 0; i + (pow2(k) - 1) < n; ++i) {
-                table[i][k] = merge(table[i][k - 1], table[i + pow2(k - 1)][k - 1]);
+    sparse_table(vector<int> &a): n(a.size()), table(__lg(n) + 1, vector<int>(n)) {
+        for (int i = 0; i < n; ++i) table[0][i] = a[i];
+        for (int i = 1; i <= __lg(n); ++i) {
+            for (int j = 0; j + (1 << i) <= n; ++j) {
+                table[i][j] = merge(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
             }
         }
     }
 
-    int query(int l, int r) {
-        int k = log2(r - l + 1);
-        return merge(table[l][k], table[r - (pow2(k) - 1)][k]);
+    int query(int i, int j) {
+        int k = __lg(j - i + 1);
+        return merge(table[k][i], table[k][j - (1 << k) + 1]);
     }
 };
