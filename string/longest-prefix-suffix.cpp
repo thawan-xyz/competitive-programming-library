4bbb31861a529f30dd7d1c0d8f527b17b528af3b
Thawan <thawan.xyz@gmail.com>
2026-03-16 18:20:12 -0300

    string

--- string/longest-prefix-suffix.cpp
+++ string/longest-prefix-suffix.cpp
@@ -3,12 +3,8 @@ vector<int> longest_prefix_suffix(string &s) {
     vector<int> lps(n);
     for (int i = 1; i < n; ++i) {
         int j = lps[i - 1];
-        while (j != 0 and s[i] != s[j]) {
-            j = lps[j - 1];
-        }
-        if (s[i] == s[j]) {
-            j++;
-        }
+        while (j != 0 and s[i] != s[j]) j = lps[j - 1];
+        if (s[i] == s[j]) j++;
         lps[i] = j;
     }
     return lps;
