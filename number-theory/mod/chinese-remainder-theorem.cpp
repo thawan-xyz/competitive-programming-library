8a5077fa8a1c9b4675667910523d991ee418fbfb
Thawan <thawan.xyz@gmail.com>
2026-03-17 14:14:13 -0300

    number-theory

--- number-theory/mod/chinese-remainder-theorem.cpp
+++ number-theory/mod/chinese-remainder-theorem.cpp
@@ -1,23 +1,13 @@
-pair<int, int> chinese_remainder_theorem(vector<int> &r, vector<int> &mod) {
-    int n = mod.size();
+int chinese_remainder_theorem(const vector<int> &rem, const vector<int> &mod) {
+    int r = rem[0], m = mod[0];
+    for (int i = 1; i < rem.size(); ++i) {
+        int g = gcd(m, mod[i]), h = mod[i] / g;
+        if ((rem[i] - r) % g != 0) return -1;
 
-    int i = r[0];
-    int s = mod[0];
-    for (int k = 1; k < n; ++k) {
-        int j = r[k];
-        int t = mod[k];
+        auto [_, a, b] = extended_gcd(m / g, h);
 
-        int g = gcd(s, t);
-        if (i % g != j % g) return {-1, -1};
-
-        auto [_, x, y] = extended_gcd(s / g, t / g);
-        int l = lcm(s, t);
-
-        int a = ((int128(i) * (t / g) % l) * y) % l;
-        int b = ((int128(j) * (s / g) % l) * x) % l;
-
-        i = (((a + b) % l) + l) % l;
-        s = l;
+        r += m * ((((int128(rem[i] - r) / g) * a) % h + h) % h);
+        m *= h;
     }
-    return {i, s};
+    return r;
 }
