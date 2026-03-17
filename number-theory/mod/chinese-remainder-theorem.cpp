4cd487105dacefe15b73c5e98fdf832472b9747b
Thawan <thawan.xyz@gmail.com>
2026-03-17 15:25:45 -0300

    number-theory

--- number-theory/mod/chinese-remainder-theorem.cpp
+++ number-theory/mod/chinese-remainder-theorem.cpp
@@ -1,10 +1,10 @@
 int128 chinese_remainder_theorem(const vector<int> &rem, const vector<int> &mod) {
     int128 r = rem[0], m = mod[0];
     for (int i = 1; i < rem.size(); ++i) {
-        int g = gcd(int(m % mod[i]), mod[i]), h = mod[i] / g;
+        int g = gcd(static_cast<int>(m % mod[i]), mod[i]), h = mod[i] / g;
         if ((rem[i] - r) % g != 0) return -1;
 
-        auto [_, a, b] = extended_gcd(int(m / g % h), h);
+        auto [_, a, b] = extended_gcd(static_cast<int>(m / g % h), h);
 
         r += m * (((((rem[i] - r) / g) * a) % h + h) % h);
         m *= h;
