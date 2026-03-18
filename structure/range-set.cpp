d185d3a6a6b874ac92666cfc6a652258ed771337
Thawan <thawan.xyz@gmail.com>
2026-03-18 14:10:35 -0300

    structure

--- structure/range-set.cpp
+++ structure/range-set.cpp
@@ -1,89 +1,75 @@
 struct range_set {
-    static constexpr int size = 1024;
-    static constexpr int n = (size + 63) / 64;
+    using uint = __uint64_t;
 
-    uint word[n] = {};
+    int n;
+    vector<uint> data;
 
-    bool contains(int x) {
-        return word[x >> 6] & (1ULL << (x & 63));
+    range_set(int n): n(n), data((n + 63) / 64) {}
+
+    uint from(int i) {
+        return ~0ULL << (i & 63);
     }
 
-    void insert(int x) {
-        word[x >> 6] |= 1ULL << (x & 63);
+    uint until(int i) {
+        return ~0ULL >> (63 - (i & 63));
     }
 
-    void remove(int x) {
-        word[x >> 6] &= ~(1ULL << (x & 63));
+    void apply(int k, bool on, uint mask) {
+        if (on) data[k] |= mask;
+        else data[k] &= ~mask;
+    }
+    
+    void update(int l, int r, bool on) {
+        int i = l >> 6, j = r >> 6;
+        if (i == j) {
+            apply(i, on, from(l) & until(r));
+        } else {
+            apply(i, on, from(l));
+            for (int k = i + 1; k < j; ++k) apply(k, on, ~0ULL);
+            apply(j, on, until(r));
+        }
+    }
+
+    int count(int l, int r) {
+        int c = 0;
+        int i = l >> 6, j = r >> 6;
+        if (i == j) {
+            c = __builtin_popcountll(data[i] & (from(l) & until(r)));
+        } else {
+            c += __builtin_popcountll(data[i] & from(l));
+            for (int k = i + 1; k < j; ++k) c += __builtin_popcountll(data[k]);
+            c += __builtin_popcountll(data[j] & until(r));
+        }
+        return c;
     }
 
     bool empty() {
-        for (int i = 0; i < n; ++i) {
-            if (word[i]) return false;
-        }
+        for (uint mask : data) if (mask) return false;
         return true;
     }
 
-    void insert(int l, int r) {
-        int i = l >> 6;
-        int j = r >> 6;
-        if (i == j) {
-            word[i] |= (~0ULL << (l & 63)) & (~0ULL >> (63 - (r & 63)));
-        } else {
-            word[i] |= ~0ULL << (l & 63);
-            for (int k = i + 1; k < j; ++k) {
-                word[k] = ~0ULL;
-            }
-            word[j] |= ~0ULL >> (63 - (r & 63));
+    int prev(int i) {
+        for (int k = i >> 6; k >= 0; --k, i = 63) {
+            uint mask = data[k] & until(i);
+            if (mask) return (k << 6) + 63 - __builtin_clzll(mask);
         }
+        return n;
     }
 
-    void remove(int l, int r) {
-        int i = l >> 6;
-        int j = r >> 6;
-        if (i == j) {
-            word[i] &= ~((~0ULL << (l & 63)) & (~0ULL >> (63 - (r & 63))));
-        } else {
-            word[i] &= ~(~0ULL << (l & 63));
-            for (int k = i + 1; k < j; ++k) {
-                word[k] = 0;
-            }
-            word[j] &= ~(~0ULL >> (63 - (r & 63)));
+    int next(int i) {
+        for (int k = i >> 6; k < data.size(); ++k, i = 0) {
+            uint mask = data[k] & from(i);
+            if (mask) return (k << 6) + __builtin_ctzll(mask);
         }
-    }
-
-    int prev(int x) {
-        int i = x >> 6;
-        uint curr = word[i] & (~0ULL >> (63 - (x & 63)));
-        if (curr) {
-            return (i << 6) + (63 - __builtin_clzll(curr));
-        } else {
-            for (int k = i - 1; k >= 0; --k) if (word[k]) {
-                return (k << 6) + (63 - __builtin_clzll(word[k]));
-            }
-        }
-        return -1;
-    }
-
-    int next(int x) {
-        int i = x >> 6;
-        uint curr = word[i] & (~0ULL << (x & 63));
-        if (curr) {
-            return (i << 6) + __builtin_ctzll(curr);
-        } else {
-            for (int k = i + 1; k < n; ++k) if (word[k]) {
-                return (k << 6) + __builtin_ctzll(word[k]);
-            }
-        }
-        return size;
+        return n;
     }
 
     int segments() {
-        int total = 0;
-        for (int i = 0; i < n; ++i) {
-            int count = __builtin_popcountll((word[i] >> 1) & ~word[i]);
-            if ((word[i] & 1ULL) and (i == 0 or not (word[i - 1] & (1ULL << 63)))) count += 1;
-            total += count;
+        int s = 0;
+        for (int k = 0; k < data.size(); ++k) {
+            s += __builtin_popcountll((data[k] >> 1) & ~data[k]);
+            if ((data[k] & 1) and (k == 0 or (data[k - 1] >> 63) == 0)) s += 1;
         }
-        return total;
+        return s;
     }
 };
