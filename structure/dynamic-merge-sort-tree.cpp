2ef440cae6730a7320e05812f7e4a2f4a03ad0e6
thawan-xyz <thawan.xyz@gmail.com>
2026-03-18 22:36:38 -0300

    structure

---
+++ structure/dynamic-merge-sort-tree.cpp
@@ -0,0 +1,30 @@
+struct merge_sort_tree {
+    int n;
+    vector<int> a;
+    vector<ordered_set<pair<int, int>>> tree;
+
+    merge_sort_tree(vector<int> &a): n(a.size()), a(a), tree(2 * n) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + n; j > 0; j >>= 1) {
+                tree[j].insert({a[i], i});
+            }
+        }
+    }
+
+    void update(int i, int x) {
+        for (int j = i + n; j > 0; j >>= 1) {
+            tree[j].erase({a[i], i});
+            tree[j].insert({x, i});
+        }
+        a[i] = x;
+    }
+
+    int query(int i, int j, int x) {
+        int c = 0;
+        for (i += n, j += n + 1; i < j; i >>= 1, j >>= 1) {
+            if (i & 1) c += tree[i++].order_of_key({x + 1, 0});
+            if (j & 1) c += tree[--j].order_of_key({x + 1, 0});
+        }
+        return c;
+    }
+};
--- structure/merge-sort-tree.cpp
+++ structure/merge-sort-tree.cpp
@@ -1,29 +1,17 @@
 struct merge_sort_tree {
     int n;
-    vector<int> a;
-    vector<ordered_set<pair<int, int>>> tree;
+    vector<vector<int>> tree;
 
-    merge_sort_tree(vector<int> &a): n(a.size()), a(a), tree(2 * n) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + n; j > 0; j >>= 1) {
-                tree[j].insert({a[i], i});
-            }
-        }
+    merge_sort_tree(vector<int> &a): n(a.size()), tree(2 * n) {
+        for (int i = 0; i < n; ++i) tree[i + n] = {a[i]};
+        for (int i = n - 1; i > 0; --i) merge(tree[i << 1].begin(), tree[i << 1].end(), tree[(i << 1) | 1].begin(), tree[(i << 1) | 1].end(), back_inserter(tree[i]));
     }
 
-    void update(int i, int x) {
-        for (int j = i + n; j > 0; j >>= 1) {
-            tree[j].erase({a[i], i});
-            tree[j].insert({x, i});
-        }
-        a[i] = x;
-    }
-
-    int query(int i, int j, int k) {
+    int query(int i, int j, int x) {
         int c = 0;
         for (i += n, j += n + 1; i < j; i >>= 1, j >>= 1) {
-            if (i & 1) c += tree[i++].order_of_key({k + 1, 0});
-            if (j & 1) c += tree[--j].order_of_key({k + 1, 0});
+            if (i & 1) c += upper_bound(tree[i].begin(), tree[i].end(), x) - tree[i].begin(), i++;
+            if (j & 1) --j, c += upper_bound(tree[j].begin(), tree[j].end(), x) - tree[j].begin();
         }
         return c;
     }
