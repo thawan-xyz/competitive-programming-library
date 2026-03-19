2e599ca5cc07331e1944e485e11c2e0e6ad5499f
Thawan <thawan.xyz@gmail.com>
2026-03-19 16:38:46 -0300

    graph

--- graph/tree/heavy-light-decomposition.cpp
+++ graph/tree/heavy-light-decomposition.cpp
@@ -1,63 +1,51 @@
-struct heavy_light_decomposition {
-    int n, timer = 0;
-    vector<vector<int>> &g;
-    vector<int> p, d, h;
-    segment_tree s;
-    vector<int> size, id;
+struct hld {
+    int n;
+    vector<int> p, d, h, sz, id;
+    reversable_segment_tree s, t;
 
-    heavy_light_decomposition(int r, vector<vector<int>> &g): n(g.size()), g(g), p(n), d(n), h(n), s(n), size(n), id(n) {
-        d[r] = 0;
-        dfs(r);
-        h[r] = r;
-        decompose(r);
+    hld(int r, vector<vector<int>> &g): n(g.sz()), p(n), d(n), h(n), sz(n), id(n), s(n, false), t(n, true) {
+        d[r] = 0, dfs(r, g);
+        h[r] = r, decompose(r, g, 0);
     }
 
-    void dfs(int a) {
-        size[a] = 1;
+    void dfs(int a, vector<vector<int>> &g) {
+        sz[a] = 1;
         for (int &b : g[a]) if (b != p[a]) {
-            p[b] = a;
-            d[b] = d[a] + 1;
-            dfs(b);
-            size[a] += size[b];
-            if (g[a][0] == p[a] or size[b] > size[g[a][0]]) {
+            p[b] = a, d[b] = d[a] + 1;
+            dfs(b, g), sz[a] += sz[b];
+            if (g[a][0] == p[a] or sz[b] > sz[g[a][0]]) {
                 swap(g[a][0], b);
             }
         }
     }
 
-    void decompose(int a) {
-        id[a] = ++timer;
+    int decompose(int a, vector<vector<int>> &g, int timer) {
+        id[a] = timer++;
         for (int b : g[a]) if (b != p[a]) {
-            if (b == g[a][0]) {
-                h[b] = h[a];
-            } else {
-                h[b] = b;
-            }
-            decompose(b);
+            h[b] = (b == g[a][0]) ? h[a] : b;
+            timer = decompose(b, g, timer);
         }
+        return timer;
     }
 
     int query(int a, int b) {
-        int l = s.neutral;
-        int r = s.neutral;
+        int l = s.neutral, r = s.neutral;
         while (h[a] != h[b]) {
             if (d[h[a]] > d[h[b]]) {
-                l = s.combine(l, s.query(id[h[a]], id[a]));
+                l = s.merge(l, t.query(id[h[a]], id[a]));
                 a = p[h[a]];
             } else {
-                r = s.combine(s.query_reverse(id[h[b]], id[b]), r);
+                r = s.merge(s.query(id[h[b]], id[b]), r);
                 b = p[h[b]];
             }
         }
-        if (d[a] > d[b]) {
-            l = s.combine(l, s.query(id[b], id[a]));
-        } else {
-            r = s.combine(s.query_reverse(id[a], id[b]), r);
-        }
-        return s.combine(l, r);
+        if (d[a] > d[b]) l = s.merge(l, t.query(id[b], id[a]));
+        else r = s.merge(s.query(id[a], id[b]), r);
+        return s.merge(l, r);
     }
 
-    void modify(int a, int x) {
-        s.modify(id[a], x);
+    void update(int a, int x) {
+        s.update(id[a], x);
+        t.update(id[a], x);
     }
 };
