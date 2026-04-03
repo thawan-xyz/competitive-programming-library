e03368aa8c8a65f2fafe01ee931c25a315db66b2
Thawan <thawan.xyz@gmail.com>
2026-04-03 00:33:19 -0300

    structure

--- structure/sparse-table.cpp
+++ structure/sparse-table.cpp
@@ -1,22 +1,22 @@
 struct sparse_table {
-    int n;
+    int n, l;
     vector<vector<int>> table;
 
     int merge(int x, int y) {
         return gcd(x, y);
     }
 
-    sparse_table(vector<int> &a): n(a.size()), table(__lg(n) + 1, vector<int>(n)) {
+    sparse_table(vector<int> &a): n(a.size()), l(__lg(n)), table(l + 1, vector<int>(n)) {
         for (int i = 0; i < n; ++i) table[0][i] = a[i];
-        for (int i = 1; i <= __lg(n); ++i) {
-            for (int j = 0; j + (1 << i) <= n; ++j) {
-                table[i][j] = merge(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
+        for (int k = 1; k <= l; ++k) {
+            for (int i = 0; i + (1 << k) <= n; ++i) {
+                table[k][i] = merge(table[k - 1][i], table[k - 1][i + (1 << (k - 1))]);
             }
         }
     }
 
     int query(int i, int j) {
-        int k = __lg(j - i + 1);
-        return merge(table[k][i], table[k][j - (1 << k) + 1]);
+        int k = __lg(j - i + 1), p = 1 << k;
+        return merge(table[k][i], table[k][j - p + 1]);
     }
 };
