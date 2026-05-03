66600652880e7c2b9355966f4f16263e42e73900
Thawan <thawan.xyz@gmail.com>
2026-05-02 21:09:49 -0300

    graph

--- graph/tree/heavy-light-decomposition.cpp
+++ graph/tree/heavy-light-decomposition.cpp
@@ -1,6 +1,56 @@
 struct hld {
     int n;
     vector<int> p, d, h, id;
+    segment_tree s;
+
+    hld(int r, vector<vector<int>> &g): n(g.size()), p(n), d(n), h(n), id(n), s(n) {
+        p[r] = r, d[r] = 0, dfs(r, g);
+        h[r] = r, decompose(r, g, 0);
+    }
+
+    int dfs(int a, vector<vector<int>> &g) {
+        int size = 1, max = 0;
+        for (int &b : g[a]) if (b != p[a]) {
+            p[b] = a, d[b] = d[a] + 1;
+            int curr = dfs(b, g);
+            if (curr > max) {
+                max = curr;
+                swap(g[a][0], b);
+            }
+            size += curr;
+        }
+        return size;
+    }
+
+    int decompose(int a, vector<vector<int>> &g, int i) {
+        id[a] = i++;
+        for (int b : g[a]) if (b != p[a]) {
+            h[b] = (b == g[a][0]) ? h[a] : b;
+            i = decompose(b, g, i);
+        }
+        return i;
+    }
+
+    int query(int a, int b) {
+        int r = s.neutral;
+        while (h[a] != h[b]) {
+            if (d[h[a]] < d[h[b]]) swap(a, b);
+            r = s.merge(r, s.query(id[h[a]], id[a]));
+            a = p[h[a]];
+        }
+        if (d[a] > d[b]) swap(a, b);
+        r = s.merge(r, s.query(id[a], id[b]));
+        return r;
+    }
+
+    void update(int a, int x) {
+        s.update(id[a], x);
+    }
+};
+
+struct hld {
+    int n;
+    vector<int> p, d, h, id;
     reversable_segment_tree s, t;
 
     hld(int r, vector<vector<int>> &g): n(g.size()), p(n), d(n), h(n), id(n), s(n, false), t(n, true) {
