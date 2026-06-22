09d881bcd1f5ae05f444e43ead2cfa48d525c704
Thawan <thawan.xyz@gmail.com>
2026-06-22 09:01:13 -0300

    extra

--- extra/out.cpp
+++
@@ -1,8 +0,0 @@
-template <typename T>
-ostream &operator<<(ostream &out, const vector<T> &a) {
-    for (int i = 0; i < a.size(); ++i) {
-        if (i != 0) out << ' ';
-        out << a[i];
-    }
-    return out;
-}
