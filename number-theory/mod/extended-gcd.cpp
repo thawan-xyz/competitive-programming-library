6567bd9e3e9c63d36e013aa0569b9dd606e5435a
thawan-xyz <thawan.xyz@gmail.com>
2026-06-25 23:32:53 -0300

    number-theory

--- number-theory/mod/extended-gcd.cpp
+++ number-theory/mod/extended-gcd.cpp
@@ -1,3 +1,6 @@
+// Extended GCD: finds coefficients x and y such that ax + by = gcd(a, b)
+// Time: O(log(min(a, b))) | Space: O(log(min(a, b)))
+// Note: returns [x0, y0] | all valid pairs are: x = x0 + k*(b/gcd) and y = y0 - k*(a/gcd)
 pair<int, int> extended_gcd(int a, int b) {
     if (b == 0) return {1, 0};
 
