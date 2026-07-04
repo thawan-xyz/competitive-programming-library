e8199be87b8e8a594e16ccb6f92055ee2afae96c
Thawan <thawan.xyz@gmail.com>
2026-07-04 15:10:28 -0300

    combinatorics

--- combinatorics/permutation-combination.cpp
+++ combinatorics/permutation-combination.cpp
@@ -1,9 +1,9 @@
-int nPk(int n, int k) {
+int P(int n, int k) {
     if (k < 0 or n < k) return 0;
     return fact[n] * invf[n - k] % mod;
 }
 
-int nCk(int n, int k) {
+int C(int n, int k) {
     if (k < 0 or n < k) return 0;
-    return nPk(n, k) * invf[k] % mod;
+    return P(n, k) * invf[k] % mod;
 }
