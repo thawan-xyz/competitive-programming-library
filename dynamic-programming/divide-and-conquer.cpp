490474439256ee61cc7213790b4d699082236c68
Thawan <thawan.xyz@gmail.com>
2026-07-13 20:26:07 -0300

    dynamic-programming

--- dynamic-programming/divide-and-conquer.cpp
+++ dynamic-programming/divide-and-conquer.cpp
@@ -1,25 +1,24 @@
 int divide_and_conquer() {
     vector<int> prev(n + 1, inf);
     vector<int> curr(n + 1, inf);
-    function<void(int, int, int, int)> compute = [&](int l, int r, int opt_l, int opt_r) -> void {
-        if (l <= r) {
-            int m = (l + r) / 2;
-            curr[m] = inf;
-            int opt = opt_l;
-            for (int k = opt_l; k <= min(m - 1, opt_r); ++k) {
-                int val = prev[k] + cost(k + 1, m);
-                if (val < curr[m]) {
-                    curr[m] = val;
-                    opt = k;
-                }
+    auto compute = [&](auto &self, int l, int r, int opt_l, int opt_r) -> void {
+        if (l > r) return;
+        int m = (l + r) / 2;
+        curr[m] = inf;
+        int opt = opt_l;
+        for (int k = opt_l; k <= min(m - 1, opt_r); ++k) {
+            int val = prev[k] + cost(k + 1, m);
+            if (val < curr[m]) {
+                curr[m] = val;
+                opt = k;
             }
-            compute(l, m - 1, opt_l, opt);
-            compute(m + 1, r, opt, opt_r);
         }
+        self(self, l, m - 1, opt_l, opt);
+        self(self, m + 1, r, opt, opt_r);
     };
     prev[0] = 0;
     for (int i = 1; i <= k; ++i) {
-        compute(1, n, 0, n - 1);
+        compute(compute, 1, n, 0, n - 1);
         prev = curr;
     }
     return curr[n];
