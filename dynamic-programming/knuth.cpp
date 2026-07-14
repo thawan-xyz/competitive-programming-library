d291be9a9268982f964984a5d3374f53f319a5f4
Thawan <thawan.xyz@gmail.com>
2026-07-13 21:52:38 -0300

    dynamic-programming

--- dynamic-programming/knuth.cpp
+++ dynamic-programming/knuth.cpp
@@ -1,6 +1,6 @@
 int knuth() {
-    vector<vector<int>> val(n + 5, vector<int>(n + 5));
-    vector<vector<int>> opt(n + 5, vector<int>(n + 5));
+    vector<vector<int>> val(n + 1, vector<int>(n + 1));
+    vector<vector<int>> opt(n + 1, vector<int>(n + 1));
     for (int i = 1; i <= n; ++i) {
         val[i][i] = cost(i, i);
         opt[i][i] = i;
@@ -9,7 +9,7 @@ int knuth() {
         for (int l = 1, r = len; r <= n; ++l, ++r) {
             val[l][r] = inf;
             opt[l][r] = opt[l][r - 1];
-            for (int k = opt[l][r - 1]; k <= opt[l + 1][r]; ++k) {
+            for (int k = opt[l][r - 1]; k <= min(r - 1, opt[l + 1][r]); ++k) {
                 int curr = val[l][k] + val[k + 1][r] + cost(l, r);
                 if (curr < val[l][r]) {
                     val[l][r] = curr;
