d8ca8be970a104a56e9f369f4dc6f134475fa990
Thawan <thawan.xyz@gmail.com>
2026-07-24 12:45:18 -0300

    graph

--- graph/kuhn.cpp
+++ graph/kuhn.cpp
@@ -3,11 +3,12 @@
 // Note: connects left [0..n] to right [0..m] | match[right] returns left node or -1
 vector<int> kuhn(int n, int m, vector<vector<int>> &g) {
     vector<int> match(m + 1, -1);
-    vector<bool> v(n + 1);
+    int itr = 0;
+    vector<int> v(n + 1);
 
     auto dfs = [&](auto &self, int a) -> bool {
-        if (v[a]) return false;
-        v[a] = true;
+        if (v[a] == itr) return false;
+        v[a] = itr;
         for (int b : g[a]) if (match[b] == -1 or self(self, match[b])) {
             match[b] = a;
             return true;
@@ -15,8 +16,7 @@ vector<int> kuhn(int n, int m, vector<vector<int>> &g) {
         return false;
     };
 
-    for (int a = 0; a <= n; ++a) {
-        fill(v.begin(), v.end(), false);
+    for (int a = 0; a <= n; ++a, ++itr) {
         dfs(dfs, a);
     }
     return match;
