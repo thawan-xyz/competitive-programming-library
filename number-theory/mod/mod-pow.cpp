0f02643394e848177ea1565a078b6127d6a1203f
Thawan <thawan.xyz@gmail.com>
2026-08-06 12:01:03 -0300

    number-theory

--- number-theory/mod/mod-pow.cpp
+++ number-theory/mod/mod-pow.cpp
@@ -1,5 +1,5 @@
-int mod_pow(int128 a, int e, const int mod) {
-    int128 r = 1;
+int mod_pow(int a, int e, const int mod) {
+    int r = 1;
     while (e != 0) {
         if (e & 1) r = (r * a) % mod;
         a = (a * a) % mod;
--- number-theory/prime/miller-rabin.cpp
+++ number-theory/prime/miller-rabin.cpp
@@ -1,22 +1,35 @@
+int mod_pow(int128 a, int e, const int mod) {
+    int128 r = 1;
+    while (e != 0) {
+        if (e & 1) r = (r * a) % mod;
+        a = (a * a) % mod;
+        e >>= 1;
+    }
+    return r;
+}
+
 bool composite(int n, int a, int s, int d) {
     int128 x = mod_pow(a, d, n);
     if (x == 1 or x == n - 1) return false;
     for (int i = 1; i < s; ++i) {
         x = (x * x) % n;
         if (x == n - 1) return false;
+        if (x == 1) return true;
     }
     return true;
 }
 
 bool prime(int n) {
     if (n <= 1) return false;
-    int s = 0, d = n - 1;
-    while ((d & 1) == 0) {
-        d >>= 1;
-        s += 1;
+    static const int P[] = {2, 3, 5, 13, 19, 73, 193, 407521, 299210837};
+    for (int p : P) {
+        if (n == p) return true;
+        if (n % p == 0) return false;
     }
-    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
-        if (n == a) return true;
+    int s = __builtin_ctzll(n - 1); 
+    int d = (n - 1) >> s;
+    static const int A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    for (int a : A) {
         if (composite(n, a, s, d)) return false;
     }
     return true;
