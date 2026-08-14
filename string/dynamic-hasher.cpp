95fe2dd5305aa39e6e3e8928edfd1ab2dfbf2d51
thawan-xyz <thawan.xyz@gmail.com>
2026-08-14 16:39:46 -0300

    string

--- string/dynamic-hasher.cpp
+++ string/dynamic-hasher.cpp
@@ -1,19 +1,19 @@
 struct dynamic_hasher {
     int n;
-    vector<hashes> tree;
+    vector<info> tree;
 
     dynamic_hasher(const string &s): n(s.length()), tree(2 * n) {
-        for (int i = 0; i < n; ++i) tree[n + i] = hashes(s[i]);
+        for (int i = 0; i < n; ++i) tree[n + i] = info(s[i]);
         for (int i = n - 1; i > 0; --i) tree[i] = combine(tree[i << 1], tree[(i << 1) | 1]);
     }
 
     void update(int i, char c) {
-        tree[i += n] = hashes(c);
+        tree[i += n] = info(c);
         for (i >>= 1; i > 0; i >>= 1) tree[i] = combine(tree[i << 1], tree[(i << 1) | 1]);
     }
 
-    hashes query(int i, int j) {
-        hashes l, r;
+    info query(int i, int j) {
+        info l, r;
         for (i += n, j += n + 1; i < j; i >>= 1, j >>= 1) {
             if (i & 1) l = combine(l, tree[i++]);
             if (j & 1) r = combine(tree[--j], r);
--- string/hasher.cpp
+++ string/hasher.cpp
@@ -1,40 +1,41 @@
-const array<int, 2> BASE = {41, 53};
-const array<int, 2> MOD = {1000000007, 1000000009};
+const array<int, 2> base = {41, 53};
+const array<int, 2> mod = {1000000007, 1000000009};
 
-vector<array<int, 2>> POW = {{1, 1}};
+const int maxn = 1e6;
+array<array<int, 2>, maxn + 1> pwr;
 
-void compute_pow(int n) {
-    while (POW.size() <= n) {
-        array<int, 2> NEXT;
-        for (int i : {0, 1}) NEXT[i] = (POW.back()[i] * BASE[i]) % MOD[i];
-        POW.push_back(NEXT);
+void init() {
+    pwr[0] = {1, 1};
+    for (int i = 1; i <= maxn; ++i) {
+        for (int j = 0; j <= 1; ++j) {
+            pwr[i][j] = (pwr[i - 1][j] * base[j]) % mod[j];
+        }
     }
 }
 
-struct hashes {
+struct info {
     int len;
     array<int, 2> ord, rev;
 
-    hashes(char c = 0) {
+    info(char c = 0) {
         len = c != 0;
-        for (int i : {0, 1}) {
-            ord[i] = c % MOD[i];
-            rev[i] = c % MOD[i];
+        for (int i = 0; i <= 1; ++i) {
+            ord[i] = c % mod[i];
+            rev[i] = c % mod[i];
         }
     }
 
-    bool operator==(const hashes &o) const {
+    bool operator==(const info &o) const {
         return len == o.len and ord == o.ord;
     }
 };
 
-hashes combine(const hashes &l, const hashes &r) {
-    compute_pow(max(l.len, r.len));
-    hashes m;
+info combine(const info &l, const info &r) {
+    info m;
     m.len = l.len + r.len;
-    for (int i : {0, 1}) {
-        m.ord[i] = (l.ord[i] * POW[r.len][i] + r.ord[i]) % MOD[i];
-        m.rev[i] = (r.rev[i] * POW[l.len][i] + l.rev[i]) % MOD[i];
+    for (int i = 0; i <= 1; ++i) {
+        m.ord[i] = (l.ord[i] * pwr[r.len][i] + r.ord[i]) % mod[i];
+        m.rev[i] = (r.rev[i] * pwr[l.len][i] + l.rev[i]) % mod[i];
     }
     return m;
 }
@@ -45,23 +46,22 @@ struct hasher {
     
     hasher(string &s): n(s.length()), pref(n + 1), suf(n + 2) {
         for (int i = 0; i < n; ++i) {
-            for (int j : {0, 1}) {
-                pref[i + 1][j] = (pref[i][j] * BASE[j] + s[i]) % MOD[j];
-                suf[n - i][j] = (suf[n - i + 1][j] * BASE[j] + s[n - i - 1]) % MOD[j];
+            for (int j = 0; j <= 1; ++j) {
+                pref[i + 1][j] = (pref[i][j] * base[j] + s[i]) % mod[j];
+                suf[n - i][j] = (suf[n - i + 1][j] * base[j] + s[n - i - 1]) % mod[j];
             }
         }
     }
 
-    hashes query(int l, int r) const {
+    info query(int l, int r) const {
         assert(l <= r);
-        hashes h;
+        info h;
         h.len = r - l + 1;
-        compute_pow(h.len);
-        for (int i : {0, 1}) {
-            h.ord[i] = (pref[r + 1][i] - pref[l][i] * POW[h.len][i]) % MOD[i];
-            if (h.ord[i] < 0) h.ord[i] += MOD[i];
-            h.rev[i] = (suf[l + 1][i] - suf[r + 2][i] * POW[h.len][i]) % MOD[i];
-            if (h.rev[i] < 0) h.rev[i] += MOD[i];
+        for (int i = 0; i <= 1; ++i) {
+            h.ord[i] = (pref[r + 1][i] - pref[l][i] * pwr[h.len][i]) % mod[i];
+            h.ord[i] = (h.ord[i] + mod[i]) % mod[i];
+            h.rev[i] = (suf[l + 1][i] - suf[r + 2][i] * pwr[h.len][i]) % mod[i];
+            h.rev[i] = (h.rev[i] + mod[i]) % mod[i];
         }
         return h;
     }
