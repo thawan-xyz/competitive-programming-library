198c671e6b801229ebd3c3f649b454d4beb145a9
Thawan <thawan.xyz@gmail.com>
2026-08-19 18:41:45 -0300

    number-theory

--- number-theory/prime/pollard-rho.cpp
+++ number-theory/prime/pollard-rho.cpp
@@ -10,17 +10,28 @@ int f(int128 x, int c, const int mod) {
 int pollard_rho(int n) {
     if ((n & 1) == 0) return 2;
     if (miller_rabin(n)) return n;
-    int x = 2, y = 2, c = 1, d = 1;
-    while (d == 1) {
-        x = f(x, c, n);
-        y = f(f(y, c, n), c, n);
-        d = gcd(abs(x - y), n);
-        if (d == n) {
-            x = uniform_int_distribution<int>(2, n - 2)(rng);
-            y = x;
-            c = uniform_int_distribution<int>(1, n - 1)(rng);
-            d = 1;
+    int c = uniform_int_distribution<int>(1, n - 1)(rng);
+    int x = 2, y = 2, g = 1, l = 1, p = 2, q = 1;
+    while (g == 1) {
+        y = x;
+        int k = 0;
+        while (k < l and g == 1) {
+            p = x;
+            for (int i = 1; i <= min<int>(128, l - k); ++i) {
+                x = f(x, c, n);
+                q = ((int128) q * abs(x - y)) % n;
+            }
+            g = gcd(q, n);
+            k += 128;
         }
+        l *= 2;
     }
-    return d;
+    if (g == n) {
+        do {
+            p = f(p, c, n);
+            g = gcd(abs(p - y), n);
+        } while (g == 1);
+    }
+    if (g == n) return pollard_rho(n);
+    return g;
 }
