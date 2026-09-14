7fc110279fccdd0120551c70034212e68d172c40
Thawan <thawan.xyz@gmail.com>
2026-09-13 23:45:11 -0300

    geometry

--- geometry/polygon.cpp
+++ geometry/polygon.cpp
@@ -8,3 +8,21 @@ float polygon_area(const vector<point> &p) {
     for (int i = 0; i < n; ++i) area += cross(p[i], p[(i + 1) % n]);
     return abs(area) / 2.0;
 }
+
+bool equal_above(point p, point a) {
+    return p.y >= a.y;
+}
+
+bool crosses_ray(point p, point q, point a) {
+    return (equal_above(q, a) - equal_above(p, a)) * orient(a, p, q) > 0;
+}
+
+bool cutting_ray_test(const vector<point> &p, point a, bool strict) {
+    int c = 0;
+    int n = p.size();
+    for (int i = 0; i < n; ++i) {
+        if (on_segment(p[i], p[(i + 1) % n], a)) return not strict;
+        c += crosses_ray(p[i], p[(i + 1) % n], a);
+    }
+    return c & 1;
+}
