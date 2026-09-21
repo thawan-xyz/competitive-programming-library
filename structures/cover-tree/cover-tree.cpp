9b03ed20006188c27208262b1135d8d2d619e576
Thawan <thawan.xyz@gmail.com>
2026-09-20 21:14:43 -0300

    structures

--- structures/cover-tree/cover-tree.cpp
+++ structures/cover-tree/cover-tree.cpp
@@ -31,3 +31,65 @@ struct cover_tree {
         return len[1];
     }
 };
+
+struct cover_tree {
+    int n;
+    vector<int> all, cnt, curr, xs;
+
+    void build(int p, int l, int r) {
+        all[p] = xs[r] - xs[l];
+        if (l + 1 == r) return;
+        int m = (l + r) / 2;
+        build(2 * p, l, m);
+        build(2 * p + 1, m, r);
+    }
+
+    void recalc(int p, int l, int r) {
+        if (cnt[p] > 0) {
+            curr[p] = all[p];
+        } else if (l + 1 == r) {
+            curr[p] = 0;
+        } else {
+            curr[p] = curr[2 * p] + curr[2 * p + 1];
+        }
+    }
+
+    cover_tree(vector<int> &x): n(x.size() - 1), all(4 * n), cnt(4 * n), curr(4 * n), xs(x) {
+        build(1, 0, n);
+    }
+
+    void update(int p, int l, int r, int ql, int qr, int val) {
+        if (qr <= l or r <= ql) return;
+        if (ql <= l and r <= qr) {
+            cnt[p] += val;
+            recalc(p, l, r);
+            return;
+        }
+        int m = (l + r) / 2;
+        update(2 * p, l, m, ql, qr, val);
+        update(2 * p + 1, m, r, ql, qr, val);
+        recalc(p, l, r);
+    }
+
+    int query(int p, int l, int r, int ql, int qr, int c) {
+        if (qr <= l or r <= ql) return 0;
+        if (ql <= l and r <= qr) {
+            if (cnt[p] > 0 or c > 0) return all[p];
+            return curr[p];
+        }
+        int m = (l + r) / 2;
+        return query(2 * p, l, m, ql, qr, c + cnt[p]) + query(2 * p + 1, m, r, ql, qr, c + cnt[p]);
+    }
+
+    void update(int l, int r, int v) {
+        update(1, 0, n, l, r, v);
+    }
+
+    int query(int l, int r) {
+        return query(1, 0, n, l, r, 0);
+    }
+
+    int query() {
+        return curr[1];
+    }
+};
